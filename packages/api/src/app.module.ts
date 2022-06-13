a6acdf834e1191561901e1e3a5550103bc2d40bf
Vshpak3 <vadimshpak3@gmail.com>
2022-06-13 19:16:53 -0400

    add in auth module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/user/user.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [UsersModule]
