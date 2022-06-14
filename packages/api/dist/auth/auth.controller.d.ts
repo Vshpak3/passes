5735bc1b9b7b1f7e18164ad08a3d1dca2c7a73b7
Vshpak3 <vadimshpak3@gmail.com>
2022-06-13 20:21:19 -0400

    add more to auth model

--- packages/api/dist/auth/auth.controller.d.ts
+++ packages/api/dist/auth/auth.controller.d.ts
@@ -1,8 +1,9 @@
+import { Request } from 'express';
 import { AuthService } from './auth.service';
 export declare class AuthController {
     private authService;
     constructor(authService: AuthService);
-    signup(): {
+    signup(req: Request): {
         msg: string;
     };
     signin(): {
