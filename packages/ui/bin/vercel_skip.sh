fbe96f50eef4e316114cc0d81622127ba600a121
Vshpak3 <vadimshpak3@gmail.com>
2022-07-08 15:11:06 -0400

    Check that vercel runs (#45)

--- packages/ui/bin/vercel_skip.sh
+++ packages/ui/bin/vercel_skip.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Ensures we only deploy Vercel on branches "preview". and "main".
+# Ensures we only deploy Vercel when files under `packages/ui/` are modified.
 # This script is specified in the Vercel settings page.
 #
 
@@ -14,9 +14,13 @@ function cancel() {
     exit 0
 }
 
+
+echo "Path: ${PWD}"
+
 # Output all Vercel Git environment variables
 # Documented here: https://vercel.com/docs/concepts/git/vercel-for-gitlab
 cat <<EOT
+
 VERCEL_GIT_PROVIDER: ${VERCEL_GIT_PROVIDER}
 VERCEL_GIT_REPO_SLUG: ${VERCEL_GIT_REPO_SLUG}
 VERCEL_GIT_REPO_OWNER: ${VERCEL_GIT_REPO_OWNER}
@@ -26,13 +30,11 @@ VERCEL_GIT_COMMIT_SHA: ${VERCEL_GIT_COMMIT_SHA}
 VERCEL_GIT_COMMIT_MESSAGE: ${VERCEL_GIT_COMMIT_MESSAGE}
 VERCEL_GIT_COMMIT_AUTHOR_LOGIN: ${VERCEL_GIT_COMMIT_AUTHOR_LOGIN}
 VERCEL_GIT_COMMIT_AUTHOR_NAME: ${VERCEL_GIT_COMMIT_AUTHOR_NAME}
+
 EOT
 
-echo "Path: ${PWD}"
-echo
-
 # Checks if the UI directory was modified
-if ! git diff HEAD^ HEAD --quiet -- .; then
+if ! git diff HEAD^ HEAD --quiet -- . ; then
     proceed
 else
     cancel
