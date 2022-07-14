f52624ef7d73ed9773ad0353866b1dbc74b1c1aa
Vshpak3 <vadimshpak3@gmail.com>
2022-07-14 13:44:19 -0400

    add redis mocks to gem and redislock services (#71)
    * add redis mocks to gem and redislock services
    
    * lint fixes

--- packages/api/src/modules/gem/gem.service.spec.ts
+++ packages/api/src/modules/gem/gem.service.spec.ts
@@ -1,11 +1,9 @@
 import { EntityManager } from '@mikro-orm/core'
 import { getRepositoryToken } from '@mikro-orm/nestjs'
 import { Test, TestingModule } from '@nestjs/testing'
-import { RedisModule } from '@nestjs-modules/ioredis'
 
-import { redisOptions } from '../../database/redis.options'
 import { repositoryMockFactory } from '../../database/test-helpers'
-import { RedisLockModule } from '../redisLock/redisLock.module'
+import { RedisLockService } from '../redisLock/redisLock.service'
 import { GemBalanceEntity } from './entities/gem.balance.entity'
 import { GemTransactionEntity } from './entities/gem.transaction.entity'
 import { GemService } from './gem.service'
@@ -15,11 +13,14 @@ describe('GemService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [RedisLockModule, RedisModule.forRootAsync(redisOptions)],
       providers: [
         GemService,
         {
           provide: EntityManager,
+          useFactory: jest.fn(() => ({})),
+        },
+        {
+          provide: RedisLockService,
           useFactory: jest.fn(() => ({})),
         },
         {
--- packages/api/src/modules/redisLock/redisLock.service.spec.ts
+++ packages/api/src/modules/redisLock/redisLock.service.spec.ts
@@ -1,20 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { RedisModule } from '@nestjs-modules/ioredis'
+import { getRedisConnectionToken } from '@nestjs-modules/ioredis'
 
-import { redisOptions } from '../../database/redis.options'
 import { RedisLockService } from './redisLock.service'
 describe('RedisLockService', () => {
   let service: RedisLockService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [RedisModule.forRootAsync(redisOptions)],
-      providers: [RedisLockService],
+      providers: [
+        RedisLockService,
+        {
+          provide: getRedisConnectionToken(),
+          useFactory: jest.fn(() => ({})),
+        },
+      ],
     }).compile()
 
     service = module.get<RedisLockService>(RedisLockService)
   })
-
   it('should be defined', () => {
     expect(service).toBeDefined()
   })
