1a7fbc24e40053ced94f8d97e92ede9e76120a87
Vshpak3 <vadimshpak3@gmail.com>
2022-07-19 13:11:41 -0400

    fix to spec (#95)

--- packages/api/src/modules/gem/gem.controller.spec.ts
+++ packages/api/src/modules/gem/gem.controller.spec.ts
@@ -1,4 +1,4 @@
-import { EntityManager } from '@mikro-orm/core'
+import { EntityManager } from '@mikro-orm/mysql'
 import { getRepositoryToken } from '@mikro-orm/nestjs'
 import { Test, TestingModule } from '@nestjs/testing'
 
