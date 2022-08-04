576fcdbf06ae1a925aee73dd3f146e34107a790c
Vshpak3 <vadimshpak3@gmail.com>
2022-08-03 23:36:21 -0400

    Remove extra sentry logging

--- packages/api/src/monitoring/sentry/sentry.options.ts
+++ packages/api/src/monitoring/sentry/sentry.options.ts
@@ -4,16 +4,11 @@ import { SentryModuleOptions } from '@ntegral/nestjs-sentry'
 export const sentryOptions = {
   useFactory: async (
     configService: ConfigService,
-  ): Promise<SentryModuleOptions> =>
-    (function tmpLog(): SentryModuleOptions {
-      let options = {
-        dsn: configService.get('monitoring.sentry_dsn'),
-        environment: configService.get('infra.env'),
-        tracesSampleRate: 1.0,
-        debug: false,
-      }
-      console.log(options)
-      return options
-    })(),
+  ): Promise<SentryModuleOptions> => ({
+    dsn: configService.get('monitoring.sentry_dsn'),
+    environment: configService.get('infra.env'),
+    tracesSampleRate: 1.0,
+    debug: false,
+  }),
   inject: [ConfigService],
 }
