f3dd2228f6c24fef1a3307f9c2f570236d911194
Vshpak3 <vadimshpak3@gmail.com>
2022-08-21 19:45:20 -0400

    Add back app e2e test

--- packages/api/src/app.spec.ts
+++ packages/api/src/app.spec.ts
@@ -1,30 +1,41 @@
 /* eslint jest/expect-expect: ["error", { "assertFunctionNames": ["expect", "request.**.expect"] }] */
 
-import type { INestApplication } from '@nestjs/common'
-import { Test, TestingModule } from '@nestjs/testing'
+import { getMikroORMToken } from '@mikro-orm/nestjs'
+import { HttpStatus, INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
 import { getRedisConnectionToken } from '@nestjs-modules/ioredis'
 import request from 'supertest'
 
 import { AppModule } from './../src/app.module'
+import { getDatabaseProviderToken } from './database/database.provider'
+import { contextNames } from './database/mikro-orm.options'
 
 // TODO: enable once we move to knex
 // eslint-disable-next-line jest/no-disabled-tests
-describe.skip('App e2e', () => {
+describe('App e2e', () => {
   let app: INestApplication
-  let moduleFixture: TestingModule
+
+  const overrides = [
+    // Mock database
+    ...contextNames.map((n) => getMikroORMToken(n)),
+    ...contextNames.map((n) => getDatabaseProviderToken(n)),
+    // Mock redis
+    getRedisConnectionToken(),
+  ]
 
   beforeAll(async () => {
-    moduleFixture = await Test.createTestingModule({
+    const moduleBuilder = Test.createTestingModule({
       imports: [AppModule],
     })
-      // Mock database
-      // .overrideProvider(['MikroORM'])
-      // .useValue({})
-      // Mock redis
-      .overrideProvider(getRedisConnectionToken())
-      .useValue({})
-      .compile()
 
+    overrides.forEach((override) =>
+      moduleBuilder
+        .overrideProvider(override)
+        // Needed so the OnApplicationShutdown for MikroORM doesn't break app.close()
+        .useValue({ close: () => undefined }),
+    )
+
+    const moduleFixture = await moduleBuilder.compile()
     app = moduleFixture.createNestApplication()
     app.setGlobalPrefix('api', { exclude: [''] })
 
@@ -38,7 +49,7 @@ describe.skip('App e2e', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/api/health')
-      .expect(200)
+      .expect(HttpStatus.OK)
       .expect('healthy')
   })
 })
