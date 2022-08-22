9385005dfa7c6860f2f5f6f02b32619da173ecbc
Vshpak3 <vadimshpak3@gmail.com>
2022-08-21 20:24:17 -0400

    Remove empty links from carousel

--- packages/ui/src/components/molecules/CarouselCard.js
+++ packages/ui/src/components/molecules/CarouselCard.js
@@ -3,22 +3,18 @@ import Image from "next/image"
 const CarouselCard = ({ name, img, org, id }) => (
   <div id={id} className="carousel-item relative p-3">
     <div className="dark:bg-jacarta-700 dark:border-jacarta-700 border-jacarta-100 block rounded-3xl border bg-white p-5 transition-shadow hover:shadow-lg">
-      <a href="#">
-        <Image
-          src={img}
-          alt="item 4"
-          width="230"
-          height="230"
-          className="w-full rounded-[0.625rem]"
-          loading="lazy"
-        />
-      </a>
+      <Image
+        src={img}
+        alt={name}
+        width="230"
+        height="230"
+        className="w-full rounded-[0.625rem]"
+        loading="lazy"
+      />
       <div className="mt-4 flex items-center justify-between">
-        <a href="#">
-          <span className="font-display text-jacarta-700 text-base hover:text-[#8358ff] dark:text-white">
-            {name}
-          </span>
-        </a>
+        <span className="font-display text-jacarta-700 text-base hover:text-[#8358ff] dark:text-white">
+          {name}
+        </span>
       </div>
       <div className="mt-2 text-sm">
         <span className="dark:text-jacarta-300">{org}</span>
