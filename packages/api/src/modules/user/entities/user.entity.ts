521a0841cbd6c5e4d2f23e3a42c93abfeadd233b
Vshpak3 <vadimshpak3@gmail.com>
2022-08-24 10:45:35 -0400

    Add in lengths for user entity

--- packages/api/src/modules/user/entities/user.entity.ts
+++ packages/api/src/modules/user/entities/user.entity.ts
@@ -1,40 +1,51 @@
 import { Entity, Index, Property, Unique } from '@mikro-orm/core'
 
 import { BaseEntity } from '../../../database/base-entity'
+import {
+  USER_COUNTRY_CODE_LENGTH,
+  USER_DISPLAY_NAME_LENGTH,
+  USER_EMAIL_LENGTH,
+  USER_LEGAL_FULL_NAME_LENGTH,
+  USER_OAUTH_ID_LENGTH,
+  USER_OAUTH_PROVIDER_LENGTH,
+  USER_PASSWORD_HASH_LENGTH,
+  USER_PHONE_NUMBER_LENGTH,
+  USER_USERNAME_LENGTH,
+} from '../constant/schema'
 
-@Entity({ tableName: 'users' }) // not a good idea to have a table named "user" in postgres
+@Entity({ tableName: 'users' }) // not a good idea to have a table named "user" in mysql
 @Index({ properties: ['oauthId', 'oauthProvider'] })
 export class UserEntity extends BaseEntity<
   'isKYCVerified' | 'isCreator' | 'isDisabled'
 > {
-  @Property({ length: 255 })
+  @Property({ length: USER_EMAIL_LENGTH })
   email: string
 
-  @Property({ length: 255 })
+  @Property({ length: USER_PASSWORD_HASH_LENGTH })
   passwordHash?: string
 
-  @Property({ length: 255 })
+  @Property({ length: USER_OAUTH_ID_LENGTH })
   oauthId?: string
 
-  @Property({ length: 255 })
+  @Property({ length: USER_OAUTH_PROVIDER_LENGTH })
   oauthProvider?: string
 
-  @Property({ length: 30 })
+  @Property({ length: USER_USERNAME_LENGTH })
   @Index()
   @Unique()
   username: string
 
-  @Property({ length: 50 })
+  @Property({ length: USER_LEGAL_FULL_NAME_LENGTH })
   legalFullName?: string
 
-  @Property({ length: 50 })
+  @Property({ length: USER_DISPLAY_NAME_LENGTH })
   @Index()
   displayName?: string
 
-  @Property({ length: 255 })
+  @Property({ length: USER_PHONE_NUMBER_LENGTH })
   phoneNumber?: string
 
-  @Property({ length: 255 })
+  @Property({ length: USER_COUNTRY_CODE_LENGTH })
   countryCode?: string
 
   @Property({ type: 'date' })
