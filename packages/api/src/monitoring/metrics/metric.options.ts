0f69a086323c2a8b15b5076bd5c96fb95877397e
Vshpak3 <vadimshpak3@gmail.com>
2022-08-29 20:04:18 -0400

    Fix datatdog metrics issue

--- packages/api/src/monitoring/metrics/metric.options.ts
+++ packages/api/src/monitoring/metrics/metric.options.ts
@@ -6,9 +6,12 @@ export const metricOptions = {
   useFactory: async (
     configService: ConfigService,
   ): Promise<MetricsModuleOptions> => ({
-    // Use default host and port (sends to local daemon)
     globalTags: { env: configService.get('infra.env') as string },
     mock: configService.get('infra.env') === 'dev',
+    // Send metrics to the local daemon. This host must match the docker gateway
+    // ip in order properly hit localhost outside of the docker container
+    host: '172.17.0.1',
+    port: 8125,
   }),
   inject: [ConfigService],
 }
