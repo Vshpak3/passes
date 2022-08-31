80b42041a29f09a8a5e5689a6735a51298da694f
Vshpak3 <vadimshpak3@gmail.com>
2022-08-31 16:54:32 -0400

    TODO comment for auth

--- packages/api/src/util/auth.util.ts
+++ packages/api/src/util/auth.util.ts
@@ -8,6 +8,7 @@ export async function redirectAfterSuccessfulLogin(
 ) {
   const accessToken = this.jwtAuthService.createAccessToken(user)
   const refreshToken = this.jwtRefreshService.createRefreshToken(user.id)
+  // TODO: only generate signed cookies for creators
   await this.s3Service.signCookies(res, `*/${user.id}`)
   return res.redirect(
     this.configService.get('clientUrl') +
