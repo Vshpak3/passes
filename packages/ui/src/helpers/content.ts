f313b22623836ce6ce57fdb659e37a0cf0eeb410
Vshpak3 <vadimshpak3@gmail.com>
2022-09-06 12:42:13 -0400

    Add in content helper

---
+++ packages/ui/src/helpers/content.ts
@@ -0,0 +1,20 @@
+class Content {
+
+  static profileImage(userId: string, profileId: string): string {
+    return `${process.env.NEXT_PUBLIC_CDN_URL}/profile/${userId}/profile-${profileId}.jpg`
+  }
+
+  static profileBanner(userId: string, profileId: string): string {
+    return `${process.env.NEXT_PUBLIC_CDN_URL}/profile/${userId}/banner-${profileId}.jpg`
+  }
+
+  static passImage(userId: string, passId: string): string {
+    return `${process.env.NEXT_PUBLIC_CDN_URL}/pass/${userId}/${passId}.jpg`
+  }
+
+  static nftJson(collectionId: string, nftId: string): string {
+    return `${process.env.NEXT_PUBLIC_CDN_URL}/nft/${nftId}.json`
+  }
+}
+
+export default Content
