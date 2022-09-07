770fc1d196a7dfbb4f029ebf28a165f2e27978ea
Vshpak3 <vadimshpak3@gmail.com>
2022-09-06 20:44:05 -0400

    Make optional parameters actually optional

--- packages/api/src/web/dto.web.ts
+++ packages/api/src/web/dto.web.ts
@@ -1,7 +1,9 @@
 import { applyDecorators } from '@nestjs/common'
 import { ApiProperty, ApiPropertyOptions } from '@nestjs/swagger'
 import { Expose } from 'class-transformer'
+import { IsOptional } from 'class-validator'
 
 export function DtoProperty(options?: ApiPropertyOptions) {
-  return applyDecorators(Expose(), ApiProperty(options))
+  const isOptional = options?.required === false ? [IsOptional] : []
+  return applyDecorators(Expose(), ApiProperty(options), ...isOptional)
 }
