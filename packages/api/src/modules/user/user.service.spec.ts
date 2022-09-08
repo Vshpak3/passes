abcda478a14bf73f6e1f1f7625caf33626b09f49
Vshpak3 <vadimshpak3@gmail.com>
2022-09-08 16:23:57 -0400

    Fix tests for user service

--- packages/api/src/modules/user/user.service.spec.ts
+++ packages/api/src/modules/user/user.service.spec.ts
@@ -2,13 +2,21 @@ import { Test, TestingModule } from '@nestjs/testing'
 
 import { getBaseProviders } from '../../util/providers.test'
 import { UserService } from './user.service'
+import { JwtAuthService } from '../auth/jwt/jwt-auth.service'
 
 describe('UserService', () => {
   let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, ...getBaseProviders()],
+      providers: [
+        UserService,
+        ...getBaseProviders(),
+        {
+          provide: JwtAuthService,
+          useFactory: jest.fn(() => ({})),
+        },
+      ],
     }).compile()
 
     service = module.get<UserService>(UserService)
