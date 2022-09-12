603adc2a660fb248934981cda2912dbead8dcee8
Vshpak3 <vadimshpak3@gmail.com>
2022-09-12 13:19:26 -0700

    Remove unnecessary override for ui eslint

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -9,19 +9,14 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:react/recommended"
   ],
-  overrides: [
-    {
-      files: ["./src/**/*"],
-      rules: {
-        // Breaking for some reason after upgrades
-        "react/no-unknown-property": "off",
-        "react/prop-types": "off",
-        "react/react-in-jsx-scope": "off",
-        "sonarjs/cognitive-complexity": "off",
-        "sonarjs/no-duplicate-string": "off"
-      }
-    }
-  ],
+  rules: {
+    // Breaking for some reason after upgrades
+    "react/no-unknown-property": "off",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "sonarjs/cognitive-complexity": "off",
+    "sonarjs/no-duplicate-string": "off"
+  },
   // Easier to run JS scripts than have build require tsc-node
   ignorePatterns: ["*.config.js", "scripts/generate-robots-txt.js"]
 }
