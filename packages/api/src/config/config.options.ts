2619a05dd5375b8b72e8d90fba38e7932573c65a
Vshpak3 <vadimshpak3@gmail.com>
2022-09-22 15:37:02 -0400

    Fix env files for dev

--- packages/api/src/config/config.options.ts
+++ packages/api/src/config/config.options.ts
@@ -4,8 +4,8 @@ import { configConfiguration, configValidationSchema } from './config.schema'
 
 // Once the config module is instantiated it will remove all system environment
 // variables (due to the setting ignoreEnvVars: true). We need to pull out the
-// variables we care about before this happen Since configs get evaluated at
-// import-time (that's when the module is created), we need to do this here
+// variables we care about before this happen. Since configs get evaluated at
+// import-time (that's when the module is created), we need to do this here.
 export const infra_config_node_env = process.env.NODE_ENV ?? 'dev'
 export const infra_config_aws_region = process.env.AWS_REGION
 // eslint-disable-next-line no-console
@@ -14,7 +14,9 @@ console.log(`Set by infra:
   - AWS_REGION: ${infra_config_aws_region}`)
 
 export const configOptions = {
-  envFilePath: path.join(__dirname, `.env.${infra_config_node_env}`),
+  envFilePath: path
+    .join(__dirname, `.env.${infra_config_node_env}`)
+    .replace('dist/', infra_config_node_env !== 'dev' ? 'dist/' : ''),
   load: [configConfiguration],
   validationSchema: configValidationSchema,
   isGlobal: true,
