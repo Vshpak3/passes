85708c8dd33502a8b8b404c7137a7e2e5a257268
Vshpak3 <vadimshpak3@gmail.com>
2022-09-23 16:00:10 -0400

    Add in docs and docker compose

--- bin/setup-local-s3-bucket.sh
+++ bin/setup-local-s3-bucket.sh
@@ -9,4 +9,6 @@ set -o pipefail
 readonly container_name="passes-s3"
 readonly bucket_name="passes"
 
+docker compose up --detach --no-recreate s3
+
 docker exec ${container_name} awslocal s3 mb s3://${bucket_name}
