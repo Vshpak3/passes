46e37f1dc357ba00db400e48450f9d4a6a3b6eb0
Vshpak3 <vadimshpak3@gmail.com>
2022-09-26 20:03:11 -0300

    removed mocked data on infinite scroll pagination

--- packages/ui/src/components/pages/profile/main-content/news-feed/creator-content-feed.js
+++ packages/ui/src/components/pages/profile/main-content/news-feed/creator-content-feed.js
@@ -3,40 +3,15 @@ import InfiniteScroll from "react-infinite-scroll-component"
 
 import { Post } from "./post"
 
-const mockPost = {
-  numLikes: 1400,
-  numComments: 338,
-  sharesCount: 220,
-  locked: true,
-  price: 32,
-  date: "2022-09-01T11:52:25.712Z",
-  displayName: "Alex Drachnik",
-  username: "alexdrachnik",
-  caption:
-    "I’m so excited to share EXACTLY how I made these TikToks for Insomniac go viral. I show how I experimented, the videos, and explain the process for making engaged Tiktoks.",
-  content: [
-    { url: "/pages/profile/profile-post-photo.png", type: "image/jpeg" }
-  ]
-}
-
 const CreatorContentFeed = ({ existingPosts, ownsProfile }) => {
-  const [posts, setPosts] = useState([...existingPosts])
-  const [hasMore] = useState(true)
-
-  const getMorePost = async () => {
-    const newPosts = new Array(10).fill(1).map(() => mockPost)
-    setPosts((post) => [...post, ...newPosts])
-  }
+  // TODO: implement pagination with infinite scroll, and loading state
+  const [posts] = useState([...existingPosts])
 
   return (
     <Fragment>
       <div className="w-full">
         <InfiniteScroll
           dataLength={posts.length}
-          next={getMorePost}
-          hasMore={hasMore}
-          loader={<h3> Loading...</h3>}
-          endMessage={<h4>Nothing more to show</h4>}
           className="w-full"
           style={{ width: "100%" }}
         >
