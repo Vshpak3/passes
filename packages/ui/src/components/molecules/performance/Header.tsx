72302641d10b128bcfa8d0e1323de02a9770419c
Vshpak3 <vadimshpak3@gmail.com>
2022-09-28 15:25:14 -0400

    update settings top navigation (#588)

--- packages/ui/src/components/molecules/performance/Header.tsx
+++ packages/ui/src/components/molecules/performance/Header.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image"
-import ChatIcon from "public/icons/chat.svg"
-import ChevronDown from "public/icons/chevron-down-icon.svg"
 import CreatorSearchBar from "src/layout/CreatorSearchBar"
 
 const Header = () => {
@@ -10,22 +7,6 @@ const Header = () => {
         <div className="hidden md:block">
           <CreatorSearchBar />
         </div>
-        <button className="flex items-center space-x-2.5 rounded-md bg-[#1b141d]/50 px-5 font-medium">
-          <ChatIcon />
-          <span>Chat</span>
-        </button>
-        <button className="flex items-center space-x-3">
-          <div className="relative h-[34px] w-[34px]">
-            <Image
-              layout="fill"
-              src="/img/user.png"
-              alt="user profile img"
-              objectFit="cover"
-              objectPosition="center"
-            />
-          </div>
-          <ChevronDown />
-        </button>
       </div>
     </div>
   )
