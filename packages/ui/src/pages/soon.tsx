ef7d2d3a0fa6e770789e42b59478a1140f58b33a
Vshpak3 <vadimshpak3@gmail.com>
2022-09-30 18:10:47 -0300

    added styling to /soon

--- packages/ui/src/pages/soon.tsx
+++ packages/ui/src/pages/soon.tsx
@@ -1,7 +1,7 @@
 export const ComingSoon = () => (
-  <div className="flex w-full items-center justify-center pt-[60px]">
+  <div className="flex h-screen w-full items-center justify-center pt-[60px]">
     <div className="flex w-[500px] flex-col items-center justify-center gap-[15px] text-center">
-      <h1 className="mt-4 text-[100px] font-bold leading-[80px] text-black">
+      <h1 className="mt-4 text-[70px] font-bold leading-[80px] text-white">
         Coming Soon
       </h1>
     </div>
