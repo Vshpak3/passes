1b6cc93bcefbf5039bd52c0538e686e8e2fbdc13
Vshpak3 <vadimshpak3@gmail.com>
2022-09-30 17:22:20 -0400

    Temporarily force prod

--- packages/ui/src/helpers/env.ts
+++ packages/ui/src/helpers/env.ts
@@ -1,3 +1,6 @@
 export const isDev = process.env.NEXT_PUBLIC_NODE_ENV === "dev"
 export const isStaging = process.env.NEXT_PUBLIC_NODE_ENV === "stage"
-export const isProd = process.env.NEXT_PUBLIC_NODE_ENV === "prod"
+export const isProd = true
+
+// temporary
+console.log(process.env)
