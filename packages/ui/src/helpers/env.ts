651f566caf3c2331deb0bb04f112adba8e38ab27
Vshpak3 <vadimshpak3@gmail.com>
2022-09-30 17:31:35 -0400

    Log full process.env.NEXT_PUBLIC_NODE_ENV

--- packages/ui/src/helpers/env.ts
+++ packages/ui/src/helpers/env.ts
@@ -3,4 +3,4 @@ export const isStaging = process.env.NEXT_PUBLIC_NODE_ENV === "stage"
 export const isProd = true
 
 // temporary
-console.log(process.env)
+console.log(process.env.NEXT_PUBLIC_NODE_ENV)
