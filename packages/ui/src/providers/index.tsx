a4d81e4d055c3f3dd752eae81f0307297aa11eea
Vshpak3 <vadimshpak3@gmail.com>
2022-10-01 00:07:15 -0400

    Remove error message

--- packages/ui/src/providers/index.tsx
+++ packages/ui/src/providers/index.tsx
@@ -8,8 +8,6 @@ const Providers = ({ children, Component, pageProps }: any) => {
       {providers.reduceRight(
         (accumulator: any, Provider: any) => (
           <Provider Component={Component} pageProps={pageProps}>
-            shorthand propert... Remove this comment to see the full error
-            message
             {accumulator}
           </Provider>
         ),
