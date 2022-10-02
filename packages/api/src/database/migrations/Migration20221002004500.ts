1ad3fbd1ad4d8415f3c1cbb7fd81b69b37d7a577
Vshpak3 <vadimshpak3@gmail.com>
2022-10-02 05:20:46 -0400

    remove broken migration

--- packages/api/src/database/migrations/Migration20221002004500.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20221002004500 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('alter table `creator_stat` drop column `show_media_count`;');
-  }
-
-}
