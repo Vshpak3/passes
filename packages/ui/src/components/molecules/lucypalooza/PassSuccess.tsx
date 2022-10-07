d7217d9b4fd2be4c7b087a91a8f4e5b83cfce84c
Vshpak3 <vadimshpak3@gmail.com>
2022-10-06 20:12:40 -0400

    add pass to success

--- packages/ui/src/components/molecules/lucypalooza/PassSuccess.tsx
+++ packages/ui/src/components/molecules/lucypalooza/PassSuccess.tsx
@@ -3,17 +3,18 @@ import React from "react"
 import { Button } from "src/components/atoms"
 
 interface IPassSuccess {
+  title: string
   passId: string
   tokenId: string
 }
 
-const PassSuccess = ({ passId, tokenId }: IPassSuccess) => {
+const PassSuccess = ({ title, passId, tokenId }: IPassSuccess) => {
   const router = useRouter()
   return (
     <div className="mx-auto mt-[93px] flex max-w-[960px] items-center justify-between space-x-[93px]">
       <div className="flex flex-col items-center space-y-9 text-center">
         <h4 className="text-2xl font-bold leading-[25px]">
-          Your Pass Was Purchased Successfully!
+          Your {title} Was Purchased Successfully!
         </h4>
         <p className="text-[17px] leading-[22px]">
           Thank you! We will see you at Lucypalooza on Wednesday, October 12.
