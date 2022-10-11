037d9a8bb6bc43bef1340d2d08ecea4a5ac7ff94
Vshpak3 <vadimshpak3@gmail.com>
2022-10-11 03:14:33 +0100

    fix: profile image preview max size, add to mobile (#738)

--- packages/ui/src/components/organisms/profile/profile-details/ProfileDetails.tsx
+++ packages/ui/src/components/organisms/profile/profile-details/ProfileDetails.tsx
@@ -57,10 +57,10 @@ const ProfileDetails: FC<ProfileDetailsProps> = ({
         />
       )}
       <Modal isOpen={isProfilePicModalOpen} setOpen={setIsProfilePicModalOpen}>
-        <div className="">
+        <div className="flex flex-row justify-center">
           <img
             src={ContentService.profileImage(profile.userId)}
-            className="object-cover drop-shadow-profile-photo"
+            className="max-w-[500px] object-cover drop-shadow-profile-photo"
             alt=""
             onError={({ currentTarget }) => {
               currentTarget.onerror = null
@@ -100,7 +100,10 @@ const ProfileDetails: FC<ProfileDetailsProps> = ({
 
         {/* Mobile */}
         <div className="align-center my-4 -mt-[220px] flex grid w-full content-center items-center justify-items-center gap-y-[16px] rounded-[20px] border border-[#ffffff]/10 bg-[#1b141d]/50 px-[16px] py-[13px] backdrop-blur-[100px] md:hidden">
-          <ProfileImage userId={profile.userId} />
+          <ProfileImage
+            userId={profile.userId}
+            onClick={() => setIsProfilePicModalOpen(true)}
+          />
           {/* TODO: fix styling for mobile */}
           {ownsProfile && <EditProfileAction onEditProfile={onEditProfile} />}
           <ProfileInformationMobile
