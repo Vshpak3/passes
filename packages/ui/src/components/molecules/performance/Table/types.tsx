9c8a52aeca1d3d51786b2862acfce544b227c07a
Vshpak3 <vadimshpak3@gmail.com>
2022-10-13 02:15:12 +0800

    remove performance dashboard (#771)

--- packages/ui/src/components/molecules/performance/Table/types.tsx
+++
@@ -1,7 +0,0 @@
-export enum Filters {
-  "most-recent" = "most-recent",
-  "most-earned" = "most-earned",
-  "most-reached" = "most-reached",
-  "most-purchased" = "most-purchased",
-  "highest-price" = "highest-price"
-}
