4e47414c8f637494d67fa8a4ea9ce3147f0f4094
Vshpak3 <vadimshpak3@gmail.com>
2022-10-18 12:54:27 -0400

    reformat

--- packages/api/src/database/migrations/Migration20221018163322.ts
+++ packages/api/src/database/migrations/Migration20221018163322.ts
@@ -1,13 +1,15 @@
-import { Migration } from '@mikro-orm/migrations';
+import { Migration } from '@mikro-orm/migrations'
 
 export class Migration20221018163322 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('alter table `pass_holder` modify `access_type` enum(\'pass_access\', \'account_access\') not null;');
+    this.addSql(
+      "alter table `pass_holder` modify `access_type` enum('pass_access', 'account_access') not null;",
+    )
   }
 
   async down(): Promise<void> {
-    this.addSql('alter table `pass_holder` modify `access_type` enum(\'pass access\', \'account_access\') not null;');
+    this.addSql(
+      "alter table `pass_holder` modify `access_type` enum('pass access', 'account_access') not null;",
+    )
   }
-
 }
