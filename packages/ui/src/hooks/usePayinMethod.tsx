d5d7c0673719caee6cffe65db9d6eb09d8bcc8b4
Vshpak3 <vadimshpak3@gmail.com>
2022-10-20 12:33:10 -0400

    fix payin method buggyness

--- packages/ui/src/hooks/usePayinMethod.tsx
+++ packages/ui/src/hooks/usePayinMethod.tsx
@@ -1,6 +1,9 @@
 import { CircleCardDto, PayinMethodDto, PaymentApi } from "@passes/api-client"
 import { useEffect, useState } from "react"
-import useSWR from "swr"
+import useSWR, { useSWRConfig } from "swr"
+
+const CACHE_KEY_PAYIN_METHOD = "/payment/default-payin-method"
+const CACHE_KEY_CARDS = "/payment/cards"
 
 export const usePayinMethod = () => {
   const api = new PaymentApi()
@@ -9,7 +12,7 @@ export const usePayinMethod = () => {
     data: payinMethod,
     isValidating: isLoadingPayinMethod,
     mutate: mutatePayinMethod
-  } = useSWR<PayinMethodDto>("/payment/default-payin-method", async () => {
+  } = useSWR<PayinMethodDto>(CACHE_KEY_PAYIN_METHOD, async () => {
     return await api.getDefaultPayinMethod()
   })
 
@@ -17,26 +20,41 @@ export const usePayinMethod = () => {
     data: cards,
     isValidating: isLoadingCards,
     mutate: mutateCards
-  } = useSWR<CircleCardDto[]>("/payment/cards", async () => {
+  } = useSWR<CircleCardDto[]>(CACHE_KEY_CARDS, async () => {
     setTimeout(() => undefined, 500)
     return (await api.getCircleCards()).cards
   })
 
   const [defaultCard, setDefaultCard] = useState<CircleCardDto>()
 
+  const { mutate: _mutateManual } = useSWRConfig()
+  const mutateManualPayinMethod = (update: PayinMethodDto) =>
+    _mutateManual(CACHE_KEY_PAYIN_METHOD, update, {
+      populateCache: (update: PayinMethodDto) => {
+        return update
+      },
+      revalidate: false
+    })
+  const mutateManualCard = (update: CircleCardDto[]) =>
+    _mutateManual(CACHE_KEY_CARDS, update, {
+      populateCache: (update: CircleCardDto[]) => {
+        return update
+      },
+      revalidate: false
+    })
+
   async function setDefaultPayinMethod(dto: PayinMethodDto) {
     await api.setDefaultPayinMethod({
       setPayinMethodRequestDto: dto
     })
-    setTimeout(() => undefined, 500)
-    mutatePayinMethod()
+    mutateManualPayinMethod(dto)
   }
 
   async function deleteCard(cardId: string) {
     await api.deleteCircleCard({
       circleCardId: cardId
     })
-    mutateCards()
+    mutateManualCard(cards?.filter((card) => card.id !== cardId) ?? [])
   }
 
   useEffect(() => {
