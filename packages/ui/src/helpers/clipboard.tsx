e777068fba7d605b3ae699835ffef58d4363ca99
Vshpak3 <vadimshpak3@gmail.com>
2022-10-20 10:58:19 -0400

    Standardize toast

--- packages/ui/src/helpers/clipboard.tsx
+++ packages/ui/src/helpers/clipboard.tsx
@@ -1,17 +1,8 @@
 import copy from "copy-to-clipboard"
-import ms from "ms"
 import { toast } from "react-toastify"
 
 export const copyLinkToClipboard = (username: string, postId: string) => {
   copy(window.location.origin + "/" + username + "/" + postId)
 
-  toast("Link to post has been copied to clipboard!", {
-    position: "bottom-left",
-    autoClose: ms("3 seconds"),
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined
-  })
+  toast.success("Link to post has been copied to clipboard")
 }
