cffd389600e713be124ca6426509a337894d7ac5
Vshpak3 <vadimshpak3@gmail.com>
2022-10-21 16:51:35 +0300

    fix: fix carousel error (#865)

--- packages/ui/src/components/atoms/PostImage.tsx
+++ packages/ui/src/components/atoms/PostImage.tsx
@@ -1,28 +1,28 @@
 import { ContentDto } from "@passes/api-client"
-import { FC, RefObject } from "react"
+import { FC, ForwardedRef, forwardRef } from "react"
 import { ContentService } from "src/helpers/content"
 
 export interface PostImageProps {
   content: ContentDto
-  ref: RefObject<HTMLImageElement>
   onMediaLoad?: () => () => void
+  ref: ForwardedRef<HTMLImageElement>
 }
 
-export const PostImage: FC<PostImageProps> = ({
-  content,
-  ref,
-  onMediaLoad
-}) => {
-  return (
-    <img
-      ref={ref}
-      onLoad={onMediaLoad}
-      key={content.contentId}
-      src={ContentService.userContentMediaPath(content)}
-      alt=""
-      height={400}
-      width={650}
-      className="h-[400px] w-full select-none rounded-[20px] object-cover"
-    />
-  )
-}
+export const PostImage: FC<PostImageProps> = forwardRef(
+  ({ content, onMediaLoad }, ref: ForwardedRef<HTMLImageElement>) => {
+    return (
+      <img
+        ref={ref}
+        onLoad={onMediaLoad}
+        key={content.contentId}
+        src={ContentService.userContentMediaPath(content)}
+        alt=""
+        height={400}
+        width={650}
+        className="h-[400px] w-full select-none rounded-[20px] object-cover"
+      />
+    )
+  }
+)
+
+PostImage.displayName = "PostImage"
--- packages/ui/src/components/molecules/PostContent.tsx
+++ packages/ui/src/components/molecules/PostContent.tsx
@@ -1,5 +1,5 @@
 import { ContentDtoContentTypeEnum } from "@passes/api-client"
-import { FC } from "react"
+import { FC, ForwardedRef, forwardRef } from "react"
 import { PostImage, PostImageProps } from "src/components/atoms/PostImage"
 import { ContentService } from "src/helpers/content"
 
@@ -7,37 +7,40 @@ import { PostVideo } from "./post/PostVideo"
 
 export interface PostContentProps extends PostImageProps {
   setPostHandler?: () => void
+  ref: ForwardedRef<HTMLImageElement>
 }
 
-export const PostContent: FC<PostContentProps> = ({
-  content,
-  ref,
-  onMediaLoad,
-  setPostHandler
-}) => {
-  let contentElement: JSX.Element | undefined
+export const PostContent: FC<PostContentProps> = forwardRef(
+  (
+    { content, setPostHandler, onMediaLoad },
+    ref: ForwardedRef<HTMLImageElement>
+  ) => {
+    let contentElement: JSX.Element | undefined
 
-  switch (content.contentType) {
-    case ContentDtoContentTypeEnum.Image:
-      contentElement = (
-        <PostImage content={content} ref={ref} onMediaLoad={onMediaLoad} />
-      )
-      break
-    case ContentDtoContentTypeEnum.Video:
-      contentElement = (
-        <PostVideo
-          key={content.contentId}
-          videoUrl={ContentService.userContentMediaPath(content)}
-        />
-      )
-      break
+    switch (content.contentType) {
+      case ContentDtoContentTypeEnum.Image:
+        contentElement = (
+          <PostImage content={content} ref={ref} onMediaLoad={onMediaLoad} />
+        )
+        break
+      case ContentDtoContentTypeEnum.Video:
+        contentElement = (
+          <PostVideo
+            key={content.contentId}
+            videoUrl={ContentService.userContentMediaPath(content)}
+          />
+        )
+        break
+    }
+
+    return contentElement ? (
+      <button className="w-full" onClick={setPostHandler}>
+        {contentElement}
+      </button>
+    ) : (
+      <></>
+    )
   }
+)
 
-  return contentElement ? (
-    <button className="w-full" onClick={setPostHandler}>
-      {contentElement}
-    </button>
-  ) : (
-    <></>
-  )
-}
+PostContent.displayName = "PostContent"
