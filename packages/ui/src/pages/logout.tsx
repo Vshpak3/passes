21b13af87d3ed4ba7ff876d1e68936f56b7c2126
Vshpak3 <vadimshpak3@gmail.com>
2022-10-22 20:39:48 -0400

    clearing cookies

--- packages/ui/src/pages/logout.tsx
+++ packages/ui/src/pages/logout.tsx
@@ -2,11 +2,22 @@ import { useEffect } from "react"
 import { useSafeRouter } from "src/hooks/useSafeRouter"
 import { useUser } from "src/hooks/useUser"
 
+function deleteAllCookies() {
+  const cookies = document.cookie.split(";")
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i]
+    const eqPos = cookie.indexOf("=")
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
+  }
+}
 const Logout = () => {
   const { safePush } = useSafeRouter()
   const { logout } = useUser()
 
   useEffect(() => {
+    deleteAllCookies()
     logout()
 
     safePush("/login")
