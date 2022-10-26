a1ff079537363580d9e71b0f6d406229c9b43b91
Vshpak3 <vadimshpak3@gmail.com>
2022-10-25 20:37:32 -0400

    fix with login layout

--- packages/ui/src/layout/WithLoginPageLayout.tsx
+++ packages/ui/src/layout/WithLoginPageLayout.tsx
@@ -15,18 +15,22 @@ const LoginWrapper: FC<PropsWithChildren<LoginWrapperProps>> = ({
   routeOnlyIfAuth
 }) => {
   const [ready, setReady] = useState(false)
-  const { userClaims } = useUser()
+  const { userClaims, user, mutate } = useUser()
   const router = useRouter()
   const { safePush } = useSafeRouter()
 
   useEffect(() => {
-    if (router.isReady) {
+    if (router.isReady && user) {
       const redirected = authRouter(safePush, userClaims, routeOnlyIfAuth)
       setReady(!redirected)
     }
     // This is intended to only run on page load to ensure direction from page
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router])
+
+  useEffect(() => {
+    mutate()
+  }, [mutate])
 
   return <>{ready ? children : <div className="flex-2 h-screen bg-black" />}</>
 }
