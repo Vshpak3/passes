3331711f39af98933c2b5df4a10fb392a79fb989
Vshpak3 <vadimshpak3@gmail.com>
2022-10-26 19:58:29 -0400

    Change message

--- packages/ui/src/components/organisms/home/Hero.tsx
+++ packages/ui/src/components/organisms/home/Hero.tsx
@@ -58,8 +58,7 @@ export const Hero = () => {
             alt="Palooza"
           />
           <p className="max-w-2xl text-center text-lg font-medium">
-            Monetize your following by creating clean, exclusive,
-            behind-the-scenes content on Passes.
+            Welcome to Passes.
           </p>
           <div className="mx-auto flex w-full max-w-lg flex-col py-8">
             <p className="text-center text-lg font-bold">Sign Up for Updates</p>
