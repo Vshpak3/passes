d8e49f216c6516b16e2988f6349a37e1f60b4947
Vshpak3 <vadimshpak3@gmail.com>
2022-10-26 20:23:43 -0400

    Final migrate fix hopefully

--- docker/migrate.sh
+++ docker/migrate.sh
@@ -18,6 +18,7 @@ readonly subnet_ids=${3}
 readonly security_group=${4}
 readonly image_name=${5}
 
+# Construct the new task definition from the old one by replacing the image
 # describe-task-definition returns additional fields that cause errors when
 # using register-task-definition so we delete those fields
 old_task_definition=$(aws ecs describe-task-definition --task-definition ${task_family})
@@ -25,32 +26,37 @@ new_task_defintiion=$(
   echo ${old_task_definition} | jq -r --arg IMAGE ${image_name} \
     '.taskDefinition |
      .containerDefinitions[0].image = $IMAGE |
-     .runtimePlatform={
-      "operatingSystemFamily": "LINUX",
-      "cpuArchitecture": "ARM64"
-     } |
      del(.taskDefinitionArn, .revision, .status, .requiresAttributes, .compatibilities, .registeredAt, .registeredBy)'
 )
 
-new_task_info=$(aws ecs register-task-definition --family ${task_family} --cli-input-json "${new_task_defintiion}")
+# Register the new task definition
+new_task_info=$(
+  aws ecs register-task-definition \
+    --family ${task_family} \
+    --runtime-platform 'operatingSystemFamily=LINUX,cpuArchitecture=ARM64' \
+    --cli-input-json "${new_task_defintiion}"
+)
 new_revision=$(echo ${new_task_info} | jq -r '.taskDefinition.revision')
 
+# Run the task
 run_result=$(
-  aws ecs run-task --cluster $cluster_name --task-definition ${task_family}:${new_revision} \
+  aws ecs run-task \
+    --cluster $cluster_name \
+    --task-definition ${task_family}:${new_revision} \
     --launch-type FARGATE \
     --network-configuration "awsvpcConfiguration={subnets=[${subnet_ids}],securityGroups=[${security_group}]}"
 )
 task_arn=$(echo ${run_result} | jq -r '.tasks[0].taskArn')
 echo "Task ARN ${task_arn}"
 
+# Wait for the task to stop
 aws ecs wait tasks-stopped --cluster ${cluster_name} --tasks ${task_arn}
 describe_result=$(aws ecs describe-tasks --cluster ${cluster_name} --tasks ${task_arn})
-
 terminated_status=$(echo ${describe_result} | jq -r '.tasks[0].containers[0].exitCode')
 
+# Check the status code and exit
 echo "Status code ${terminated_status}"
 if [[ ${terminated_status} != "0" ]] ; then
   echo ${describe_result} | jq
 fi
-
 exit ${terminated_status}
