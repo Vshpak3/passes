d12fe5279fcb06defe4f84cd50c0519371ff4769
Vshpak3 <vadimshpak3@gmail.com>
2022-10-27 23:33:10 -0400

    Fix deploys for now

--- packages/ui/src/components/organisms/sidebar/SidebarMobileWrapper.tsx
+++ packages/ui/src/components/organisms/sidebar/SidebarMobileWrapper.tsx
@@ -2,10 +2,11 @@ import { FC, PropsWithChildren, useState } from "react"
 
 import { MobileNavbar } from "src/components/molecules/Sidebar/SidebarLayout/MobileNavbar"
 import { SidebarMobileContainer } from "src/components/molecules/Sidebar/SidebarLayout/SidebarMobileContainer"
-import { useWindowSize } from "src/hooks/useWindowSizeHook"
+// import { useWindowSize } from "src/hooks/useWindowSizeHook"
 
 export const SidebarMobileWrapper: FC<PropsWithChildren> = ({ children }) => {
-  const { isTablet } = useWindowSize()
+  // const { isTablet } = useWindowSize()
+  const isTablet = false
 
   const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false)
 
