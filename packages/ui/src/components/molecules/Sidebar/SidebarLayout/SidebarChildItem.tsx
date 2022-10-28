27b6a2f992f57ef1de9f723313fed4cdb643aaf4
Vshpak3 <vadimshpak3@gmail.com>
2022-10-29 05:03:03 +0800

    Update: Sidebar dfrop down children styles (#935)

--- packages/ui/src/components/molecules/Sidebar/SidebarLayout/SidebarChildItem.tsx
+++ packages/ui/src/components/molecules/Sidebar/SidebarLayout/SidebarChildItem.tsx
@@ -19,9 +19,9 @@ export const SidebarChildItem: FC<SidebarChildItemProps> = ({
         <span
           className={classNames(
             isActive
-              ? "group ml-[-25px] cursor-pointer items-center rounded-[56px] bg-[#FFFEFF]/10 px-[25px]"
-              : "text-[#eeedef]/50 hover:text-white",
-            "group flex cursor-pointer items-center py-[10px] text-base font-semibold tracking-[0.003em] text-white"
+              ? "group ml-[-25px] cursor-pointer items-center rounded-[56px] bg-[#FFFEFF]/10 px-[25px] font-bold"
+              : "text-bg-[#FFFEFF]/10 font-normal hover:text-white",
+            "group flex cursor-pointer items-center py-[10px] text-sm tracking-[0.003em] text-white md:text-base"
           )}
         >
           {subItem.name}
