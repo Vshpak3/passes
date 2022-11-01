1697d91e5f5cfc8a333091d1450c68d141a2b356
Vshpak3 <vadimshpak3@gmail.com>
2022-10-31 22:48:07 -0400

    fix lint

--- packages/ui/src/components/organisms/profile/profile-details/ProfileThumbnail.tsx
+++ packages/ui/src/components/organisms/profile/profile-details/ProfileThumbnail.tsx
@@ -5,9 +5,7 @@ import { ProfileImageProps } from "./ProfileImage"
 
 type ProfileThumbnailProps = ProfileImageProps
 
-export const ProfileThumbnailUnmemo: FC<ProfileThumbnailProps> = ({
-  userId
-}) => {
+const ProfileThumbnailUnmemo: FC<ProfileThumbnailProps> = ({ userId }) => {
   const [loaded, setLoaded] = useState<boolean>(false)
   return (
     <div className="h-[42px] w-[42px] flex-shrink-0 select-none overflow-hidden rounded-full bg-gray-900">
