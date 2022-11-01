04838a2f7d196d675724dc861c8a5db4c34839a3
Vshpak3 <vadimshpak3@gmail.com>
2022-10-31 22:46:15 -0400

    fix image load

--- packages/ui/src/components/organisms/profile/profile-details/ProfileThumbnail.tsx
+++ packages/ui/src/components/organisms/profile/profile-details/ProfileThumbnail.tsx
@@ -1,22 +1,34 @@
-import { FC, memo } from "react"
+import { FC, memo, useState } from "react"
 
 import { ContentService } from "src/helpers/content"
 import { ProfileImageProps } from "./ProfileImage"
 
 type ProfileThumbnailProps = ProfileImageProps
 
-const ProfileThumbnailUnmemo: FC<ProfileThumbnailProps> = ({ userId }) => (
-  <div className="h-[42px] w-[42px] flex-shrink-0 select-none overflow-hidden rounded-full bg-gray-900">
-    <img
-      className="h-full w-full object-cover object-center"
-      src={ContentService.profileThumbnailPath(userId)}
-      alt="user profile thumbnail"
-      onError={({ currentTarget }) => {
-        currentTarget.onerror = null
-        currentTarget.src = "/img/profile/default-profile-img.svg"
-      }}
-    />
-  </div>
-)
+export const ProfileThumbnailUnmemo: FC<ProfileThumbnailProps> = ({
+  userId
+}) => {
+  const [loaded, setLoaded] = useState<boolean>(false)
+  return (
+    <div className="h-[42px] w-[42px] flex-shrink-0 select-none overflow-hidden rounded-full bg-gray-900">
+      <img
+        className="h-full w-full object-cover object-center"
+        src={"/img/profile/default-profile-img.svg"}
+        alt="user profile thumbnail"
+        style={loaded ? { display: "none" } : {}}
+      />
+      <img
+        className="h-full w-full object-cover object-center"
+        src={ContentService.profileThumbnailPath(userId)}
+        alt="user profile thumbnail"
+        onLoad={() => setLoaded(true)}
+        // onError={({ currentTarget }) => {
+        //   currentTarget.onerror = null
+        //   currentTarget.src = "/img/profile/default-profile-img.svg"
+        // }}
+      />
+    </div>
+  )
+}
 
 export const ProfileThumbnail = memo(ProfileThumbnailUnmemo)
