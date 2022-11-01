11c5ce557679c213ee811bc71529128b71de1331
Vshpak3 <vadimshpak3@gmail.com>
2022-10-31 23:12:30 -0400

    fix rerender on resize

--- packages/ui/src/hooks/useWindowSizeHook.tsx
+++ packages/ui/src/hooks/useWindowSizeHook.tsx
@@ -3,41 +3,30 @@ import { useEffect, useState } from "react"
 import { breakpoints } from "src/styles/breakpoints"
 
 // Define general type for useWindowSize hook, which includes width and height
-interface Size {
-  width: number | undefined
-  height: number | undefined
-}
 
 export function useWindowSize() {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState<Size>({
-    width: typeof window !== "undefined" ? window.innerWidth : 0,
-    height: typeof window !== "undefined" ? window.innerHeight : 0
-  })
-  const windowWidth =
-    windowSize && windowSize.width ? windowSize.width : breakpoints.md
-  const isMobile = windowWidth < breakpoints.md
-  const isTablet = windowWidth < breakpoints.lg
+  const [isMobile, setIsMobile] = useState<boolean>(false)
+  const [isTablet, setIsTablet] = useState<boolean>(false)
 
   useEffect(() => {
-    // Handler to call on window resize
     function handleResize() {
+      const windowWidth = window.innerWidth ?? breakpoints.md
+      setIsMobile(windowWidth < breakpoints.md)
+      setIsTablet(windowWidth < breakpoints.lg)
       // Set window width/height to state
-      setWindowSize({
-        width: window ? window.innerWidth : 0,
-        height: window ? window.innerHeight : 0
-      })
     }
+    // Handler to call on window resize
     if (typeof window !== "undefined") {
       // Add event listener
       window.addEventListener("resize", handleResize)
       // Call handler right away so state gets updated with initial window size
       handleResize()
-    }
 
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize)
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize)
+    }
   }, []) // Empty array ensures that effect is only run on mount
 
   return {
