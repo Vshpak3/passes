614128f5ebe3f123374eb9f3d824f782974c10d8
Vshpak3 <vadimshpak3@gmail.com>
2022-11-02 05:37:10 +0800

    Update: Recent tip on channel list style updated (#952)

--- packages/ui/src/components/molecules/messages/ChannelListItem.tsx
+++ packages/ui/src/components/molecules/messages/ChannelListItem.tsx
@@ -42,20 +42,24 @@ const ChannelListItemUnmemo: FC<ChannelListItemProps> = ({
         <ProfileThumbnail userId={channel.otherUserId} />
       </div>
       <div className="flex w-full justify-between">
-        <div className="flex flex-col items-start justify-start">
+        <div className="flex flex-col items-start justify-center">
           <span className="text-[16px] font-medium leading-[24px] text-white">
             <NameDisplay
               displayName={channel.otherUserDisplayName}
               username={channel.otherUserUsername}
             />
           </span>
-          <p className="w-[120px] truncate text-[14px] font-medium leading-[17px] text-[#ffff]/30">
+          <p className="w-[120px] truncate text-[14px] font-medium text-[#ffff]/30">
             {channel?.previewText ?? ""}
           </p>
         </div>
-        <div className="flex flex-col items-center justify-end gap-1">
+        <div
+          className={`flex flex-col items-center gap-1 ${
+            channel?.unreadTip !== 0 ? "justify-center" : "justify-end"
+          }`}
+        >
           {channel?.unreadTip !== 0 && (
-            <span className="w-full items-center self-end rounded-[30px] bg-[#BF7AF0] p-1 text-center text-[10px] font-medium leading-[16px] text-[#fff]">
+            <span className="w-full items-center self-end rounded-md border-2 border-[#FF51A8] p-1 text-center text-[12px] font-bold text-[#FF51A8]">
               Tip: {formatCurrency(channel?.unreadTip)}
             </span>
           )}
