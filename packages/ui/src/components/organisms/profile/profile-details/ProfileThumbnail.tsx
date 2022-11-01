f269f27c1546f6f70041681d61e33c3dba6fab69
Vshpak3 <vadimshpak3@gmail.com>
2022-11-01 01:47:53 -0400

    unmemo thumbnail

--- packages/ui/src/components/organisms/profile/profile-details/ProfileThumbnail.tsx
+++ packages/ui/src/components/organisms/profile/profile-details/ProfileThumbnail.tsx
@@ -1,11 +1,11 @@
-import { FC, memo, useState } from "react"
+import { FC, useState } from "react"
 
 import { ContentService } from "src/helpers/content"
 import { ProfileImageProps } from "./ProfileImage"
 
 type ProfileThumbnailProps = ProfileImageProps
 
-const ProfileThumbnailUnmemo: FC<ProfileThumbnailProps> = ({ userId }) => {
+export const ProfileThumbnail: FC<ProfileThumbnailProps> = ({ userId }) => {
   const [loaded, setLoaded] = useState<boolean>(false)
   return (
     <div className="h-[42px] w-[42px] flex-shrink-0 select-none overflow-hidden rounded-full bg-gray-900">
@@ -24,5 +24,3 @@ const ProfileThumbnailUnmemo: FC<ProfileThumbnailProps> = ({ userId }) => {
     </div>
   )
 }
-
-export const ProfileThumbnail = memo(ProfileThumbnailUnmemo)
