ddd71a10972206b03da65a10f682207829afc892
Vshpak3 <vadimshpak3@gmail.com>
2022-11-03 00:44:35 +0800

    update: channel stream style fixed (#958)

--- packages/ui/src/components/molecules/messages/ChannelView.tsx
+++ packages/ui/src/components/molecules/messages/ChannelView.tsx
@@ -65,7 +65,7 @@ export const ChannelView: FC<ChannelViewProps> = ({
     selectedChannel?.otherUserId ?? ""
   )
   return (
-    <div className="absolute z-50 flex h-[90vh] flex-col bg-[#120C14] lg:relative lg:max-h-[90vh] lg:flex-1">
+    <div className="absolute z-50 flex h-[90vh] w-full flex-col bg-[#120C14] lg:relative lg:max-h-[90vh] lg:flex-1">
       {selectedChannel && (
         <>
           <ChannelHeader
