f03d325f81a97cdf47590850871d88e547b5b914
Vshpak3 <vadimshpak3@gmail.com>
2022-11-02 23:02:50 -0400

    Security header change

--- packages/ui/next.config.js
+++ packages/ui/next.config.js
@@ -2,21 +2,10 @@
 
 const env = process.env.NEXT_PUBLIC_NODE_ENV
 
-// Prevent cross-site scripting (XSS), clickjacking and other code injection
-// attacks. Content Security Policy (CSP) can specify allowed origins for
-// content including scripts, stylesheets, images, fonts, objects, media
-// (audio, video), iframes, and more.
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self';
-  child-src ${process.env.NEXT_PUBLIC_UI_BASE_URL} ${process.env.NEXT_PUBLIC_API_BASE_URL};
-  style-src 'self' 'unsafe-inline';
-  font-src 'self';
-`
-// This is more secure but we cannot because of Tailwind:
-// style-src 'self' ${process.env.NEXT_PUBLIC_UI_BASE_URL};
-
-// https://nextjs.org/docs/advanced-features/security-headers
+// Security headers (docs at https://nextjs.org/docs/advanced-features/security-headers)
+// The following headers are not considered standard / best practice:
+//  - X-DNS-Prefetch-Control (https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control)
+//  - X-XSS-Protection No longer best practice (https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection)
 const securityHeaders = [
   // This header prevents the browser from attempting to guess the type of
   // content if the Content-Type header is not explicitly set. This can prevent
@@ -33,23 +22,33 @@ const securityHeaders = [
     value: "SAMEORIGIN"
   },
 
-  // This header informs browsers it should only be accessed using HTTPS, instead
-  // of using HTTP. Using the configuration below, all present and future
-  // subdomains will use HTTPS for a max-age of 2 years. This blocks access to
-  // pages or subdomains that can only be served over HTTP.
+  // This header informs browsers it should only be accessed using HTTPS,
+  // instead of using HTTP. Using the configuration below, all present and
+  // future subdomains will use HTTPS for a max-age of 2 years. This blocks
+  // access to pages or subdomains that can only be served over HTTP.
   {
     key: "Strict-Transport-Security",
     value: "max-age=63072000; includeSubDomains; preload"
-  },
-
-  // Prevent cross-site scripting (XSS), clickjacking and other code injection attacks.
-  {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim()
   }
 
-  // X-XSS-Protection No longer best practice
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+  // Prevent cross-site scripting (XSS), clickjacking and other code injection attacks.
+  // Content Security Policy (CSP) can specify allowed origins for
+  // content including scripts, stylesheets, images, fonts, objects, media
+  // (audio, video), iframes, and more.
+  // This is more secure but we cannot because of Tailwind:
+  // style-src 'self' ${process.env.NEXT_PUBLIC_UI_BASE_URL};
+  // {
+  //   key: "Content-Security-Policy",
+  //   value: `
+  //   default-src 'self';
+  //   script-src 'self';
+  //   child-src ${process.env.NEXT_PUBLIC_UI_BASE_URL} ${process.env.NEXT_PUBLIC_API_BASE_URL};
+  //   style-src 'self' 'unsafe-inline';
+  //   font-src 'self';
+  // `
+  //     .replace(/\s{2,}/g, " ")
+  //     .trim()
+  // }
 ]
 
 const nextConfig = {
