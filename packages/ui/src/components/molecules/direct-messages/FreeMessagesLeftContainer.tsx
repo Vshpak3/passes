9ad162d7258d784028773d8326b275425f8de9e4
Vshpak3 <vadimshpak3@gmail.com>
2022-11-04 03:18:42 -0400

    text size

--- packages/ui/src/components/molecules/direct-messages/FreeMessagesLeftContainer.tsx
+++ packages/ui/src/components/molecules/direct-messages/FreeMessagesLeftContainer.tsx
@@ -9,9 +9,9 @@ export const FreeMessagesLeftContainer: FC<FreeMessagesLeftContainerProps> = ({
 }) => {
   return (
     <div className="ml-6 flex w-2/3 max-w-xl items-center justify-between gap-[10px] rounded-b-[10px] border-b border-[#FFFF]/10 bg-gradient-to-r from-[#B52A6F33] to-[#B52A6F33]/20 py-[10px] px-6 backdrop-blur-[25px]">
-      <div className="flex flex-wrap items-center gap-1 text-sm font-medium text-[#FBFBFB]">
+      <div className="flex flex-wrap items-center gap-1 text-xs font-medium text-[#FBFBFB]">
         You have
-        <span className="text-base font-medium text-[#C943A8]">
+        <span className="text-sm font-medium text-[#C943A8]">
           {freeMessages ?? "unlimited"} free
         </span>
         <span>messages left.</span>
