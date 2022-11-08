1d1be1e26726060759cc53b2f33b5677f2c099e2
Vshpak3 <vadimshpak3@gmail.com>
2022-11-08 14:28:48 -0300

    Add styling for buy message modal (#1025)
    * added styling for buy message modal
    
    * added styling for buy message modal

--- packages/ui/src/components/molecules/payment/BuyMessageButton.tsx
+++ packages/ui/src/components/molecules/payment/BuyMessageButton.tsx
@@ -1,7 +1,7 @@
 import { MessagesApi, PayinMethodDto } from "@passes/api-client"
-import classNames from "classnames"
 import React, { FC, useCallback } from "react"
 
+import { Button } from "src/components/atoms/button/Button"
 import { LandingMessageEnum } from "src/helpers/landing-messages"
 import { usePay } from "src/hooks/usePay"
 
@@ -45,17 +45,12 @@ export const BuyMessageButton: FC<BuyMessageButtonProps> = ({
   )
 
   return (
-    <button
-      className={classNames(
-        isDisabled
-          ? "flex w-full items-center justify-center rounded-full border border-solid border-passes-pink-100 bg-passes-pink-100 py-[10px] text-base font-semibold text-white opacity-[0.40]"
-          : "flex w-full items-center justify-center rounded-full border border-solid border-passes-pink-100 bg-passes-pink-100 py-[10px] text-base font-semibold text-white"
-      )}
+    <Button
       disabled={!!blocked || submitting || isDisabled}
       onClick={submit}
-      type="submit"
+      variant="pink"
     >
       {loading ? "Loading" : "Buy message"}
-    </button>
+    </Button>
   )
 }
--- packages/ui/src/components/organisms/payment/BuyMessageModal.tsx
+++ packages/ui/src/components/organisms/payment/BuyMessageModal.tsx
@@ -1,6 +1,7 @@
 import { MessageDto, PayinMethodDto } from "@passes/api-client"
 import React, { Dispatch, FC, SetStateAction, useState } from "react"
 
+import { SectionTitle } from "src/components/atoms/SectionTitle"
 import { BuyMessageButton } from "src/components/molecules/payment/BuyMessageButton"
 import { PaymentModalBody } from "src/components/molecules/payment/PaymentModalBody"
 import { Modal } from "src/components/organisms/Modal"
@@ -21,25 +22,25 @@ export const BuyMessageModal: FC<BuyMessageModalProps> = ({
   const { images, video } = contentTypeCounter(message.contents)
 
   return (
-    <Modal isOpen setOpen={() => setMessage(null)}>
-      <div className="mb-4 flex h-[115px] w-full flex-row items-end justify-between rounded bg-gradient-to-r from-[#66697B] to-[#9C9DA9] p-4">
-        <span className="max-w-[50%] self-center text-[28px] font-bold leading-8 text-white">
-          Buy Message
-        </span>
-        <span className="text-white">${message.price.toFixed(2)}</span>
-      </div>
+    <Modal
+      isOpen
+      modalContainerClassname="lg:max-w-[30%] p-4"
+      setOpen={() => setMessage(null)}
+    >
+      <SectionTitle>Buy Message</SectionTitle>
       <div>
-        <div className="my-4">
-          <span className="text-[#ffff]/70">
+        <div className="my-4 flex justify-between">
+          <span className="rounded border border-passes-dark-gray px-2 py-1 text-white">
             {!!video && `${video} videos`} {!!images && plural("photo", images)}
           </span>
-        </div>
-        <div>
-          <span className="text-[#ffff]/90">
-            This content will be unlocked and available in the chat and gallery
-            after purchase.
+          <span className="text-white">
+            Unlock for ${message.price.toFixed(2)}
           </span>
         </div>
+        <span className="my-4 flex text-passes-dark-gray">
+          This content will be unlocked and available in the messaging chat
+          purchase.
+        </span>
         <PaymentModalBody
           closeModal={() => setMessage(null)}
           price={message?.price ?? 0}
