5ba3132aa5c60fe38c46ada0f6df952107e1c5cb
Vshpak3 <vadimshpak3@gmail.com>
2022-11-08 16:11:34 -0500

    fix

---
+++ packages/api/src/database/migrations/Migration20221108211110.ts
@@ -0,0 +1,9 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20221108211110 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter table `message` add `automatic` tinyint(1) not null default false;');
+  }
+
+}
