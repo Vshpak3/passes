f5445533745baca189df28d9d1a1b1ef3b6e6279
Vshpak3 <vadimshpak3@gmail.com>
2022-11-09 11:47:45 -0500

    increase media limits

--- packages/ui/src/config/media-limits.tsx
+++ packages/ui/src/config/media-limits.tsx
@@ -3,10 +3,10 @@ import type { FileAccept } from "src/components/atoms/input/FileInput"
 export const MAX_FILE_COUNT = 20
 
 const MB = 1048576
-export const MAX_IMAGE_SIZE = 10 * MB
-export const MAX_IMAGE_SIZE_NAME = "10 megabytes"
-export const MAX_VIDEO_SIZE = 200 * MB
-export const MAX_VIDEO_SIZE_NAME = "200 megabytes"
+export const MAX_IMAGE_SIZE = 100 * MB
+export const MAX_IMAGE_SIZE_NAME = "100 megabytes"
+export const MAX_VIDEO_SIZE = 2000 * MB
+export const MAX_VIDEO_SIZE_NAME = "2000 megabytes"
 
 export const ACCEPTED_MEDIA_TYPES: FileAccept = [
   ".png",
