593c2e32a3511807fc04768b36b8cf47741b55a5
Vshpak3 <vadimshpak3@gmail.com>
2022-11-09 14:56:36 -0300

    align creator search bar in every screen (#1039)

--- packages/ui/src/pages/messages.tsx
+++ packages/ui/src/pages/messages.tsx
@@ -30,7 +30,7 @@ const MessagesPage = () => {
   return (
     <Suspense fallback={<CenterLoader />}>
       <div className="flex h-screen flex-col">
-        <div className="hidden h-16 items-center pr-2 lg:flex">
+        <div className="hidden h-16 lg:flex">
           {user?.isCreator ? (
             <div className="flex flex-1 items-center">
               {massMessage ? (
@@ -54,7 +54,7 @@ const MessagesPage = () => {
               <SectionTitle>Messages</SectionTitle>
             </div>
           )}
-          <div className="z-[1000]">
+          <div className="z-[1000] pt-2">
             <CreatorSearchBar />
           </div>
         </div>
