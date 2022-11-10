4901fb2b4a2e70f39df3d56c3ad503d1de81a29e
Vshpak3 <vadimshpak3@gmail.com>
2022-11-10 12:42:17 -0500

    revert

--- packages/ui/src/pages/_document.tsx
+++ packages/ui/src/pages/_document.tsx
@@ -4,12 +4,7 @@ class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head>
-          <meta
-            content="width=device-width, initial-scale=1.0"
-            name="viewport"
-          />
-        </Head>
+        <Head />
         <body style={{ overscrollBehaviorY: "none" }}>
           <Main />
           <NextScript />
