37c15f3d735ebefb0e211d9a39fe7fff83e340dd
Vshpak3 <vadimshpak3@gmail.com>
2022-11-11 10:27:42 -0500

    simplify pass media

--- packages/ui/src/components/atoms/passes/PassMedia.tsx
+++ packages/ui/src/components/atoms/passes/PassMedia.tsx
@@ -30,65 +30,40 @@ export const PassMedia: FC<PassMediaProps> = ({
     <div className="h-0 w-full rounded-lg bg-black from-passes-purple-100 via-[#F03368] to-[#F6B103] pt-[100%]" />
   )
 
-  if (passHolderId) {
+  if (animationType) {
+    const animationPath = passHolderId
+      ? ContentService.passHolderAnimationPath(
+          passId,
+          passHolderId,
+          animationType
+        )
+      : ContentService.passAnimationPath(passId, animationType)
     return (
       <>
-        {animationType ? (
-          <video
-            autoPlay
-            className={classNames(shouldUsePlaceholder ? "hidden" : "")}
-            loop
-            muted
-            onLoadedData={handleLoadingAsset}
-          >
-            <source
-              src={ContentService.passHolderAnimationPath(
-                passId,
-                passHolderId,
-                animationType
-              )}
-              type="video/mp4"
-            />
-          </video>
-        ) : (
-          <img
-            alt="no media exists"
-            className={classNames(shouldUsePlaceholder ? "hidden" : "")}
-            onLoad={handleLoadingAsset}
-            src={ContentService.passHolderImagePath(
-              passId,
-              passHolderId,
-              imageType
-            )}
-          />
-        )}
+        <video
+          autoPlay
+          className={classNames(shouldUsePlaceholder ? "hidden" : "")}
+          loop
+          muted
+          onLoadedData={handleLoadingAsset}
+        >
+          <source src={animationPath} type="video/mp4" />
+        </video>
         {shouldUsePlaceholder && placeHolder}
       </>
     )
   } else {
+    const imagePath = passHolderId
+      ? ContentService.passHolderImagePath(passId, passHolderId, imageType)
+      : ContentService.passImagePath(passId, imageType)
     return (
       <>
-        {animationType ? (
-          <video
-            autoPlay
-            className={classNames(shouldUsePlaceholder ? "hidden" : "")}
-            loop
-            muted
-            onLoadedData={handleLoadingAsset}
-          >
-            <source
-              src={ContentService.passAnimationPath(passId, animationType)}
-              type="video/mp4"
-            />
-          </video>
-        ) : (
-          <img
-            alt="no media exists"
-            className={classNames(shouldUsePlaceholder ? "hidden" : "")}
-            onLoad={handleLoadingAsset}
-            src={ContentService.passImagePath(passId, imageType)}
-          />
-        )}
+        <img
+          alt="no media exists"
+          className={classNames(shouldUsePlaceholder ? "hidden" : "")}
+          onLoad={handleLoadingAsset}
+          src={imagePath}
+        />
         {shouldUsePlaceholder && placeHolder}
       </>
     )
