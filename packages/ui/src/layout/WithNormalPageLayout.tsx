80f8faebe254d4aa7818776d86d985f2c1d61c2a
Vshpak3 <vadimshpak3@gmail.com>
2022-11-13 12:57:39 -0500

    gradients

--- packages/ui/src/layout/WithNormalPageLayout.tsx
+++ packages/ui/src/layout/WithNormalPageLayout.tsx
@@ -38,11 +38,17 @@ export const WithNormalPageLayout = (
     getLayout: (page: ReactElement, hasRefreshed: boolean) => (
       <div
         className={classNames(
-          options.background ? "background-gradient" : "bg-passes-black",
+          "bg-passes-black",
           "min-safe-h-screen relative w-full lg:pb-0",
           !options.noScroll && "pb-16 "
         )}
       >
+        {options.background && (
+          <>
+            <div className="background-gradient fixed right-[0vw] top-[0vh] hidden h-[20vh] w-[20vh] blur-[10vh] md:block" />
+            <div className="background-gradient fixed right-[5vw] top-[60vh] hidden h-[15vh] w-[15vh] blur-[13vh] md:block" />
+          </>
+        )}
         <div className="mx-auto block max-w-[3000px]">
           <div className="relative w-full grid-cols-10 md:grid">
             {sidebar && <Sidebar />}
