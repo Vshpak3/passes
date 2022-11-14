18a9699ef8ffab2141d2617722778b2573de2b15
Vshpak3 <vadimshpak3@gmail.com>
2022-11-14 15:31:42 -0500

    Kill glow

--- packages/ui/src/components/organisms/home/FanTips.tsx
+++ packages/ui/src/components/organisms/home/FanTips.tsx
@@ -2,7 +2,6 @@ import React from "react"
 
 export const FanTips = () => {
   return (
-    // <div className="bg-[url('/img/homepage/glow.webp')] bg-contain bg-no-repeat py-32">
     <div className="py-32">
       <h3 className="mx-auto max-w-2xl text-center text-5xl font-extrabold leading-[4rem]">
         Get paid for your time. <br />{" "}
