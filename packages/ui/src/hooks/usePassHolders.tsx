d914f706c931f4822286ac723e34681dfff6329b
Vshpak3 <vadimshpak3@gmail.com>
2022-11-14 12:36:51 -0500

    fix

--- packages/ui/src/hooks/usePassHolders.tsx
+++ packages/ui/src/hooks/usePassHolders.tsx
@@ -10,23 +10,18 @@ const api = new PassApi()
 const CACHE_KEY_PASS_HOLDERS = "/pass-holders"
 
 export const usePassHolders = (userId?: string, passId?: string) => {
-  console.log("asdf")
   const { data: passHolders } = useSWR<PassHolderDto[]>(
     [CACHE_KEY_PASS_HOLDERS, userId, passId],
     async () => {
-      console.log("run")
-      console.log("asfads")
       const res = await api.getPassHolders({
         getPassHoldersRequestDto: {
-          // holderId: userId,
+          holderId: userId,
           passId,
           order: GetPassHoldersRequestDtoOrderEnum.Desc,
           orderType: GetPassHoldersRequestDtoOrderTypeEnum.CreatedAt,
           activeOnly: true
         }
       })
-      console.log("done")
-      console.log(res)
       return res.data
     },
     { revalidateOnMount: true }
