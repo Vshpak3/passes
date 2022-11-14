44e78a60b7360ebf14f0a23bc6f2dd5b56c76e49
Vshpak3 <vadimshpak3@gmail.com>
2022-11-14 15:29:43 -0500

    Fix background on homepage

--- packages/ui/src/components/organisms/home/FanTips.tsx
+++ packages/ui/src/components/organisms/home/FanTips.tsx
@@ -2,7 +2,8 @@ import React from "react"
 
 export const FanTips = () => {
   return (
-    <div className="bg-[url('/img/homepage/glow.webp')] bg-contain bg-no-repeat py-32">
+    // <div className="bg-[url('/img/homepage/glow.webp')] bg-contain bg-no-repeat py-32">
+    <div className="py-32">
       <h3 className="mx-auto max-w-2xl text-center text-5xl font-extrabold leading-[4rem]">
         Get paid for your time. <br />{" "}
       </h3>
@@ -11,7 +12,6 @@ export const FanTips = () => {
         Prioritize your DMs.
       </h2>
 
-      <p className="mx-auto py-4 text-center" />
       <div className="mx-auto max-w-7xl px-4 py-16 sm:py-32 sm:px-6 lg:py-16 lg:px-8">
         <img alt="Fan Tips" className="mx-auto" src="/img/homepage/tips.svg" />
       </div>
