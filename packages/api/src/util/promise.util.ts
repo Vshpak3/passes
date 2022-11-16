4075cb626c29dc5ca61b9fa6d32672304c326b68
Vshpak3 <vadimshpak3@gmail.com>
2022-11-15 19:12:16 -0500

    reason to error

--- packages/api/src/util/promise.util.ts
+++ packages/api/src/util/promise.util.ts
@@ -4,6 +4,8 @@ export const rejectIfAny = <T>(promises: PromiseSettledResult<T>[]) => {
   const rejected = promises.filter((result) => result.status === 'rejected')
 
   if (rejected.length) {
-    throw new InternalServerErrorException(rejected[0])
+    throw new InternalServerErrorException(
+      (rejected[0] as PromiseRejectedResult).reason,
+    )
   }
 }
