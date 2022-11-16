ebdba08f0059cdb76b8062630498bf0f7086134f
Vshpak3 <vadimshpak3@gmail.com>
2022-11-16 01:00:36 -0500

    Login fic

--- packages/ui/src/pages/auth/success.tsx
+++ packages/ui/src/pages/auth/success.tsx
@@ -1,24 +1,33 @@
 import { useRouter } from "next/router"
 import { FC, useEffect } from "react"
 
+import { authRouter } from "src/helpers/authRouter"
 import { queryParam } from "src/helpers/query"
 import { useAuthEvent } from "src/hooks/useAuthEvent"
+import { useSafeRouter } from "src/hooks/useSafeRouter"
 
 const AuthSuccess: FC = () => {
   const router = useRouter()
   const { auth } = useAuthEvent()
+  const { safePush } = useSafeRouter()
 
   useEffect(() => {
     if (!router.isReady) {
       return
     }
 
-    auth(async () => {
-      return {
-        accessToken: queryParam(router.query.accessToken) as string,
-        refreshToken: queryParam(router.query.refreshToken) as string
+    auth(
+      async () => {
+        return {
+          accessToken: queryParam(router.query.accessToken) as string,
+          refreshToken: queryParam(router.query.refreshToken) as string
+        }
+      },
+      async () => undefined,
+      async () => {
+        authRouter(safePush, router.query.accessToken)
       }
-    })
+    )
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router])
 
