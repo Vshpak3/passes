1e226b2b78f193d118db74f36d98a0f88d97ee1e
Vshpak3 <vadimshpak3@gmail.com>
2022-11-16 21:48:30 +0100

    mobile sidebar-gradient-icon-outline-fix (#1120)

--- packages/ui/src/components/molecules/Sidebar/SidebarLayout/SidebarMobileContainer.tsx
+++ packages/ui/src/components/molecules/Sidebar/SidebarLayout/SidebarMobileContainer.tsx
@@ -10,31 +10,44 @@ export const SidebarMobileContainer: FC<
   PropsWithChildren<SidebarMobileContainerProps>
 > = ({ children, mobileSidebarOpen, toggleSidebar }) => {
   return (
-    <Transition.Root as={Fragment} show={mobileSidebarOpen}>
-      <Dialog as="div" className="relative z-40" onClose={toggleSidebar}>
-        <div className="fixed inset-0 z-40 flex">
+    <div>
+      <Transition.Root as={Fragment} show={mobileSidebarOpen}>
+        <Dialog as="div" className="relative z-40" onClose={toggleSidebar}>
           <Transition.Child
             as={Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            // leave="transition ease-in-out duration-300 transform"
-            // leaveFrom="translate-x-0"
-            // leaveTo="-translate-x-full"
+            enter="transition-opacity ease-linear duration-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-70"
+            leave="ease-out"
+            leaveFrom="translate-x-0 opacity-100"
+            leaveTo="translate-x-full opacity-0"
           >
-            <Dialog.Panel className="relative flex w-full max-w-[220px] flex-1 flex-col overflow-y-auto border-r-[1px] border-passes-gray bg-passes-black scrollbar-hide">
-              <div className="h-0 flex-1">
-                <nav className="flex flex-col items-start gap-3">
-                  {children}
-                </nav>
-              </div>
-            </Dialog.Panel>
+            <div className="fixed inset-0 bg-gradient-to-r from-passes-primary-color/10 to-black opacity-75" />
           </Transition.Child>
-          <div aria-hidden="true" className="w-14 shrink-0">
-            {/* Dummy element to force sidebar to shrink to fit close icon */}
+          <div className="fixed inset-0 z-40 flex">
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-in-out duration-100"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="ease-out"
+              leaveFrom="translate-x-0 opacity-100"
+              leaveTo="translate-x-full opacity-0"
+            >
+              <Dialog.Panel className="relative flex w-full max-w-[220px] flex-1 flex-col overflow-y-auto border-r-[1px] border-passes-gray bg-passes-black scrollbar-hide">
+                <div className="h-0 flex-1">
+                  <nav className="flex flex-col items-start gap-3">
+                    {children}
+                  </nav>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+            <div aria-hidden="true" className="w-14 shrink-0">
+              {/* Dummy element to force sidebar to shrink to fit close icon */}
+            </div>
           </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+        </Dialog>
+      </Transition.Root>
+    </div>
   )
 }
