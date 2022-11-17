47140aa3874c1ed9aae1aae377b2007413d42e34
Vshpak3 <vadimshpak3@gmail.com>
2022-11-17 17:20:37 -0300

    mobile layout tweak for cookie banner

--- packages/ui/src/components/organisms/CookieBanner.tsx
+++ packages/ui/src/components/organisms/CookieBanner.tsx
@@ -19,28 +19,29 @@ export const CookieBanner: FC<CookieBannerProps> = ({
     <div className="fixed bottom-0 left-0 z-40 w-full lg:bottom-8">
       <div
         className="relative mx-auto flex h-44
-       w-full flex-col items-start gap-5 rounded-lg bg-[#191A1F] py-6 px-4 lg:h-32 lg:max-w-[1000px] lg:flex-row lg:items-center lg:px-8"
+       w-full flex-col items-start gap-5 bg-[#191A1F] py-6 px-4 lg:h-32 lg:max-w-[1000px] lg:flex-row lg:items-center lg:rounded-lg lg:px-8"
       >
         <Text className="text-passes-gray-100 lg:flex-1">
           We use our own and third-party cookies to analyze traffic, enhance
           your experience, and for security and marketing.
         </Text>
-        <div className="flex items-center justify-between gap-2 lg:gap-4">
-          <Button
-            className="p-2 text-base font-bold text-passes-primary-color"
-            onClick={onManage}
-            variant={ButtonVariant.NONE}
-          >
-            Manage Settings
-          </Button>
-          <Button
-            className="p-2 text-base font-bold text-passes-primary-color"
-            onClick={onReject}
-            variant={ButtonVariant.NONE}
-          >
-            Reject All
-          </Button>
-
+        <div className="flex flex-row-reverse items-center justify-between gap-2 lg:flex-row lg:gap-4">
+          <div className="flex flex-col items-start lg:flex-row lg:items-center">
+            <Button
+              className="p-2 text-base font-bold text-passes-primary-color"
+              onClick={onManage}
+              variant={ButtonVariant.NONE}
+            >
+              Manage Settings
+            </Button>
+            <Button
+              className="p-2 text-base font-bold text-passes-primary-color"
+              onClick={onReject}
+              variant={ButtonVariant.NONE}
+            >
+              Reject All
+            </Button>
+          </div>
           <Button
             className="py-4 px-2 text-base font-bold"
             icon={<UnionIcon className="mr-[5px]" />}
