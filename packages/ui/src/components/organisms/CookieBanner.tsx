3c9ef650922cb7a8bd374a826f64f434e843203b
Vshpak3 <vadimshpak3@gmail.com>
2022-11-17 15:14:32 -0300

    reworked cookie banner for mobile

--- packages/ui/src/components/organisms/CookieBanner.tsx
+++ packages/ui/src/components/organisms/CookieBanner.tsx
@@ -20,9 +20,9 @@ export const CookieBanner: FC<CookieBannerProps> = ({
   onReject
 }) => {
   return (
-    <div className="absolute bottom-10 left-0 z-20 w-full">
-      <div className="relative mx-auto flex h-[164px] w-full max-w-[1272px] items-center gap-5 rounded-[5px] bg-[#191A1F] py-[53px] px-[58px]">
-        <Text className="text-[#9D9D9D]">
+    <div className="fixed bottom-0 left-0 z-40 w-full lg:bottom-8">
+      <div className="relative mx-auto flex h-48 w-full flex-col items-center gap-5 rounded-lg bg-[#191A1F] p-6 lg:h-32 lg:max-w-[1272px] lg:flex-row lg:p-8">
+        <Text className="text-passes-gray-100">
           We use our own and third-party cookies to analyze traffic, enhance
           your experience, and for security and marketing. For more information,
           read our{" "}
@@ -31,9 +31,9 @@ export const CookieBanner: FC<CookieBannerProps> = ({
           </Link>
           .
         </Text>
-        <div className="flex items-center justify-between gap-[10px]">
+        <div className="flex items-center justify-between gap-2 lg:gap-4">
           <Button
-            className="p-[8px] text-base font-bold text-[#FF51A8]"
+            className="p-2 text-base font-bold text-[#FF51A8]"
             onClick={() => onManage()}
             variant={ButtonVariant.NONE}
           >
@@ -47,15 +47,15 @@ export const CookieBanner: FC<CookieBannerProps> = ({
             Reject All
           </Button>
           <Button
-            className="z-10 py-[19px] px-[26px] text-base font-bold"
-            icon={<UnionIcon className="z-20 mr-[5px] space-y-3" />}
+            className="py-4 px-2 text-base font-bold"
+            icon={<UnionIcon className="mr-[5px]" />}
             onClick={() => onAccept()}
           >
             Accept All
           </Button>
         </div>
         <Button
-          className="absolute right-[-20px] top-0"
+          className="absolute right-0 top-0"
           onClick={() => onClose()}
           variant={ButtonVariant.NONE}
         >
