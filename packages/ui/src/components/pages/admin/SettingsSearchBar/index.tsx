e86fc51e5dad7aa08f2462d0b56c75ba034d40f3
Vshpak3 <vadimshpak3@gmail.com>
2022-11-19 04:47:51 +0800

    Pass 1380 fe admin search bar should work like twitters (#1140)
    * update: search component for admin page updated
    
    * fix: lint

--- packages/ui/src/components/pages/admin/SettingsSearchBar/index.tsx
+++ packages/ui/src/components/pages/admin/SettingsSearchBar/index.tsx
@@ -1,61 +1,41 @@
 import { Combobox } from "@headlessui/react"
-import classNames from "classnames"
-import { ChangeEvent, FC, useCallback, useMemo, useState } from "react"
-
-import { SearchBar } from "src/components/molecules/SearchBar"
-import { AdminTabProps, AdminTabs, AdminTabsEnum } from "src/config/admin"
+import SearchIcon from "public/icons/messages-search-icon.svg"
+import React, { FC } from "react"
 
 interface SettingsSearchBar {
-  onSelect: (id: AdminTabsEnum) => void
+  value: string
+  setValue: React.Dispatch<string>
 }
 
 export const SettingsSearchBar: FC<SettingsSearchBar> = ({
-  onSelect: _onSelect
-}) => {
-  const [searchValue, setSearchValue] = useState("")
-
-  const onSelect = (id: AdminTabsEnum) => {
-    _onSelect(id)
-  }
-
-  const searchOptions = useMemo(
-    () =>
-      AdminTabs.filter((item) => item.name.includes(searchValue)).map(
-        (list: AdminTabProps) => (
-          <Combobox.Option key={list.id} value={list.id}>
-            {({ active, disabled }) => (
-              <span
-                className={classNames("block cursor-pointer py-2 px-6", {
-                  "bg-[#12070E]/90 text-passes-primary-color": active,
-                  "text-gray-500": disabled
-                })}
-              >
-                {list.name}
-              </span>
-            )}
-          </Combobox.Option>
-        )
-      ),
-    [searchValue]
-  )
-
-  const onInputChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) =>
-      setSearchValue(event.target.value),
-
-    []
-  )
-
+  value,
+  setValue
+}: SettingsSearchBar) => {
   return (
-    <div className="w-[300px]">
-      <SearchBar
-        contentName="Settings"
-        loading={false}
-        onInputChange={onInputChange}
-        onSelect={onSelect}
-        options={searchOptions}
-        searchValue={searchValue}
-      />
+    <div className="mb-2 flex h-[73px] w-full items-center border-b border-passes-dark-200 px-4">
+      <Combobox<string> onChange={setValue} value={value as string}>
+        <div className="relative flex w-full flex-col">
+          <Combobox.Button as="div">
+            <div className="relative flex items-center gap-3">
+              <SearchIcon
+                className="pointer-events-none absolute top-1/2 left-[14px] z-10 -translate-y-1/2"
+                height="20"
+                width="20"
+              />
+              <Combobox.Input
+                autoComplete="off"
+                className="form-input h-[51px] w-full rounded-md border-0 bg-[#12070E]/50 pl-11 text-white outline-none placeholder:text-[16px] placeholder:text-white/30 focus:border focus:border-passes-pink-100/80 focus:outline-none focus:ring-passes-pink-100/80"
+                id="search"
+                name="search"
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="Search Settings"
+                type="search"
+                value={value}
+              />
+            </div>
+          </Combobox.Button>
+        </div>
+      </Combobox>
     </div>
   )
 }
--- packages/ui/src/components/pages/admin/index.tsx
+++ packages/ui/src/components/pages/admin/index.tsx
@@ -2,11 +2,12 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable sonarjs/no-small-switch */
 import classNames from "classnames"
+import _ from "lodash"
 import { useRouter } from "next/router"
 import ChevronRightIcon from "public/icons/chevron-right-icon.svg"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
-import { AdminTabs, AdminTabsEnum } from "src/config/admin"
+import { AdminTabProps, AdminTabs, AdminTabsEnum } from "src/config/admin"
 import { useUser } from "src/hooks/useUser"
 import { SettingsSearchBar } from "./SettingsSearchBar"
 import { ImpersonateUser } from "./tabs/ImpersonateUser"
@@ -18,11 +19,21 @@ export const Admin = () => {
   const router = useRouter()
   const [ready, setReady] = useState(false)
   const [activeTab, setActiveTab] = useState<AdminTabsEnum>(0)
+  const [tabs, setTabs] = useState<Array<AdminTabProps>>(AdminTabs)
+  const [searchText, setSearchText] = useState<string>("")
+
+  const handleSettingsSearch = useCallback(() => {
+    setTabs(_.filter(AdminTabs, (tab) => tab.name.includes(searchText)))
+  }, [searchText])
+
+  useEffect(() => {
+    handleSettingsSearch()
+  }, [handleSettingsSearch])
+
   useEffect(() => {
     if (!router.isReady || loading) {
       return
     }
-    setReady(true)
     if (!user || !user.email.endsWith(ADMIN_EMAIL)) {
       router.push("/home")
     } else {
@@ -48,15 +59,15 @@ export const Admin = () => {
     <>
       {ready && (
         <div className="flex h-screen w-full flex-col">
-          <div className="flex w-full items-center justify-between px-2 py-4">
+          <div className="flex w-full items-center justify-between p-4">
             <span className="text-xl font-bold">Admin</span>
-            <SettingsSearchBar onSelect={handleNavItemClick} />
           </div>
           <div className=" flex flex-1 flex-row border-t-[1px] border-passes-dark-200">
-            <div className="h-full overflow-y-auto overflow-x-hidden md:block md:min-w-[330px]">
-              <div className="mx-auto h-full w-full border-passes-dark-200 pt-2 md:border-r">
+            <div className="h-full overflow-y-auto overflow-x-hidden border-passes-dark-200 md:block md:min-w-[400px] md:border-r">
+              <SettingsSearchBar setValue={setSearchText} value={searchText} />
+              <div className="mx-auto h-full w-full ">
                 <ul className="">
-                  {AdminTabs.map(({ name, id }) => (
+                  {tabs.map(({ name, id }) => (
                     <li
                       className={classNames(
                         "rounded-l-[4px] p-2.5 pr-[13px]",
