4e8f8b3d07b4f43fcba9d61179419420f32c54f2
Vshpak3 <vadimshpak3@gmail.com>
2022-11-21 18:04:10 -0500

    Fix disable corrects input

--- packages/ui/src/components/atoms/input/GeneralInput.tsx
+++ packages/ui/src/components/atoms/input/GeneralInput.tsx
@@ -65,58 +65,54 @@ export const Input: FC<InputProps> = ({
   outlineColor = "pink",
   outerClassName = "w-full",
   ...rest
-}) => {
-  const extras = disableInputCorrects
-    ? {}
-    : {
-        autocorrect: "off",
-        autocapitalize: "off",
-        spellcheck: "false"
-      }
+}) => (
+  <>
+    {!!label && <Label errors={errors} label={label} name={name} />}
 
-  return (
-    <>
-      {!!label && <Label errors={errors} label={label} name={name} />}
-
-      <div className={outerClassName}>
-        {!!icon && (
-          <div className="relative text-white">
-            <span
-              className={classNames(
-                "absolute inset-y-0 flex pl-2 pt-3 sm:px-4 sm:pr-3",
-                `${toLower(iconAlign)}-0 left-[${iconMargin}px]`
-              )}
-            >
-              {icon}
-            </span>
-          </div>
+    <div className={outerClassName}>
+      {!!icon && (
+        <div className="relative text-white">
+          <span
+            className={classNames(
+              "absolute inset-y-0 flex pl-2 pt-3 sm:px-4 sm:pr-3",
+              `${toLower(iconAlign)}-0 left-[${iconMargin}px]`
+            )}
+          >
+            {icon}
+          </span>
+        </div>
+      )}
+      <input
+        autoComplete={autoComplete}
+        placeholder={placeholder || label}
+        type={type}
+        {...register(name)}
+        {...(disableInputCorrects
+          ? {
+              autocorrect: "off",
+              autocapitalize: "off",
+              spellcheck: "false"
+            }
+          : {})}
+        {...rest}
+        className={classNames(
+          outlineColor === "pink"
+            ? "focus:border-passes-pink-100/80 focus:outline-none focus:ring-passes-pink-100/80"
+            : outlineColor === "purple"
+            ? "focus:border-passes-secondary-color/80 focus:outline-none focus:ring-passes-secondary-color/80"
+            : "",
+          transparent ? "bg-transparent" : "bg-black",
+          textPosition === "RIGHT" ? "text-right" : "text-left",
+          "block w-full appearance-none rounded-md border border-passes-dark-100 p-3",
+          "min-h-[50px] py-3 px-4 text-sm placeholder-gray-400 shadow-sm read-only:pointer-events-none read-only:bg-gray-200",
+          icon && iconAlign === EIcon.Left ? "pl-[50px]" : "pl-3",
+          className,
+          errors[name] !== undefined && "border-red-500"
         )}
-        <input
-          autoComplete={autoComplete}
-          placeholder={placeholder || label}
-          type={type}
-          {...register(name)}
-          {...extras}
-          {...rest}
-          className={classNames(
-            outlineColor === "pink"
-              ? "focus:border-passes-pink-100/80 focus:outline-none focus:ring-passes-pink-100/80"
-              : outlineColor === "purple"
-              ? "focus:border-passes-secondary-color/80 focus:outline-none focus:ring-passes-secondary-color/80"
-              : "",
-            transparent ? "bg-transparent" : "bg-black",
-            textPosition === "RIGHT" ? "text-right" : "text-left",
-            "block w-full appearance-none rounded-md border border-passes-dark-100 p-3",
-            "min-h-[50px] py-3 px-4 text-sm placeholder-gray-400 shadow-sm read-only:pointer-events-none read-only:bg-gray-200",
-            icon && iconAlign === EIcon.Left ? "pl-[50px]" : "pl-3",
-            className,
-            errors[name] !== undefined && "border-red-500"
-          )}
-        />
-        {errors && !!errors[name] && (
-          <span className="text-xs text-red-500">{errors[name].message}</span>
-        )}
-      </div>
-    </>
-  )
-}
+      />
+      {errors && !!errors[name] && (
+        <span className="text-xs text-red-500">{errors[name].message}</span>
+      )}
+    </div>
+  </>
+)
