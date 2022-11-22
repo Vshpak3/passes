a4327c2a30740fa39a74cf1cc267e80ec77b1d18
Vshpak3 <vadimshpak3@gmail.com>
2022-11-22 18:11:01 +0100

    hls disable autoLoad (#1167)
    Co-authored-by: Aaron Friedlander <aaron.abf@gmail.com>

--- packages/ui/src/components/atoms/content/VideoPlayer.tsx
+++ packages/ui/src/components/atoms/content/VideoPlayer.tsx
@@ -1,5 +1,14 @@
 import classNames from "classnames"
 import Hls from "hls.js"
+import {
+  CSSProperties,
+  Dispatch,
+  forwardRef,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState
+} from "react"
 import { CSSProperties, forwardRef, useEffect, useState } from "react"
 
 interface VideoPlayerProps {
@@ -11,8 +20,36 @@ interface VideoPlayerProps {
 }
 
 export const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
-  ({ src, autoplay = false, className = "", poster = "", style }, ref) => {
+  (
+    {
+      src,
+      autoplay = false,
+      className = "",
+      poster = "",
+      style,
+      showControls,
+      setShowControls
+    },
+    ref
+  ) => {
+    const hls = useMemo(
+      () =>
+        new Hls({
+          autoStartLoad: false,
+          // Broken until 1.2.8: https://github.com/video-dev/hls.js/issues/5015
+          enableWorker: false,
+          // Send cookies (necessary for creators)
+          xhrSetup: function (xhr) {
+            xhr.withCredentials = true
+          }
+        }),
+      []
+    )
+
     useEffect(() => {
+      if (initialized) {
+        return
+      }
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       const video = ref?.current
@@ -23,29 +60,27 @@ export const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
       // Support for standalone mp4s as opposed to HLS
       if (src.includes("-standalone.mp4")) {
         video.src = src
+        setInitialized(true)
         return
       }
 
       // Check for native browser HLS support and if not then check if HLS.js is supported
       if (video.canPlayType("application/vnd.apple.mpegurl")) {
         video.src = src
+        setInitialized(true)
       } else if (Hls.isSupported()) {
-        const hls = new Hls({
-          // TODO: PASS-1396
-          autoStartLoad: true,
-          // Broken until 1.2.8: https://github.com/video-dev/hls.js/issues/5015
-          enableWorker: false,
-          // Send cookies (necessary for creators)
-          xhrSetup: function (xhr) {
-            xhr.withCredentials = true
-          }
-        })
         hls.loadSource(src)
         hls.attachMedia(video)
+        // sets initialized = true the first time a media segment is appended to the buffer
+        hls.on(Hls.Events.BUFFER_APPENDED, () => {
+          if (!initialized) {
+            setInitialized(true)
+          }
+        })
       } else {
         console.error("No HLS support")
       }
-    }, [src, ref])
+    }, [src, ref, hls, initialized])
 
     const [showControls, setShowControls] = useState<boolean>(false)
 
@@ -57,8 +92,11 @@ export const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
         controlsList="nodownload"
         onBlur={() => setShowControls(false)}
         onFocus={() => setShowControls(true)}
-        onMouseEnter={() => setShowControls(true)}
-        onMouseLeave={() => setShowControls(false)}
+        onPlay={() => {
+          if (!initialized) {
+            hls.startLoad()
+          }
+        }}
         poster={poster}
         preload={autoplay ? "auto" : "none"}
         ref={ref}
