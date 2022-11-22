618c9ed0bcda578a1919b35a494854ddb220648a
Vshpak3 <vadimshpak3@gmail.com>
2022-11-21 20:12:08 -0500

    fix

--- packages/ui/src/components/pages/tools/analytics/AnalyticsHeader.tsx
+++ packages/ui/src/components/pages/tools/analytics/AnalyticsHeader.tsx
@@ -7,7 +7,7 @@ export const AnalyticsHeader = () => {
     <div className="flex flex-col md:flex-row">
       <div className="cover-image flex h-[137px] w-full flex-col justify-between rounded-md border border-gray-800 p-4 md:w-[373px]">
         <h4 className="text-3xl">
-          {formatCurrency(userBalance?.net.amount ?? 0)}
+          {formatCurrency(userBalance?.net?.amount ?? 0)}
         </h4>
         <div className="flex flex-row gap-[16px]">
           <p className="text-[#ccc]">Balance available</p>
