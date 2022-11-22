6027c81b8b097d8156624594c879554474243788
Vshpak3 <vadimshpak3@gmail.com>
2022-11-23 03:59:39 +0800

    chore: made create list modal wider (#1169)

--- packages/ui/src/components/molecules/list/CreateNewListModal.tsx
+++ packages/ui/src/components/molecules/list/CreateNewListModal.tsx
@@ -26,32 +26,34 @@ const CreateNewListModal: FC<CreateNewListModalProps> = ({
       onClose={() => setOpen(false)}
       open={isOpen}
     >
-      <SectionTitle>Create New List</SectionTitle>
-      <div className="relative flex flex-col pt-[18px]">
-        <input
-          className="rounded-[6px] border border-[#2C282D] bg-[#100C11] p-[10px] focus:border-passes-pink-100/80 focus:outline-none focus:ring-passes-pink-100/80"
-          onChange={handleChange}
-          placeholder="Enter List Name"
-        />
-        <div className="mt-[30px] flex flex-row justify-end gap-[20px]">
-          <Button
-            className="bg-[#c943a81a] !py-[10px] !px-[18px] font-bold text-[#C943A8]"
-            onClick={() => setOpen(false)}
-          >
-            Cancel
-          </Button>
-          <Button
-            className={classNames(
-              "bg-[#C943A8] !py-[10px] !px-[18px] font-bold text-white",
-              {
-                "!text-[#ffffffeb]": listName === ""
-              }
-            )}
-            disabled={listName === ""}
-            onClick={() => onSubmit(listName)}
-          >
-            Create
-          </Button>
+      <div className="w-full md:w-[400px]">
+        <SectionTitle>Create New List</SectionTitle>
+        <div className="relative flex flex-col pt-[18px]">
+          <input
+            className="rounded-[6px] border border-[#2C282D] bg-[#100C11] p-[10px] focus:border-passes-pink-100/80 focus:outline-none focus:ring-passes-pink-100/80"
+            onChange={handleChange}
+            placeholder="Enter List Name"
+          />
+          <div className="mt-[30px] flex flex-row justify-end gap-[20px]">
+            <Button
+              className="bg-[#c943a81a] !py-[10px] !px-[18px] font-bold text-[#C943A8]"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              className={classNames(
+                "bg-[#C943A8] !py-[10px] !px-[18px] font-bold text-white",
+                {
+                  "!text-[#ffffffeb]": listName === ""
+                }
+              )}
+              disabled={listName === ""}
+              onClick={() => onSubmit(listName)}
+            >
+              Create
+            </Button>
+          </div>
         </div>
       </div>
     </Dialog>
